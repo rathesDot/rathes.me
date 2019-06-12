0938f7cf9eca3d5bc7c34d881d0f5329b77c0075
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-12 22:48:11 +0200

    🎨 Use class components instead of functions

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,68 +1,75 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
+class SEO extends React.PureComponent {
+  render() {
+    const { description, lang, meta, title } = this.props
+
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            site {
+              siteMetadata {
+                title
+                description
+                author
+              }
+            }
           }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
-  )
+        `}
+        render={data => {
+          const metaDescription =
+            description || data.site.siteMetadata.description
+          return (
+            <Helmet
+              htmlAttributes={{
+                lang,
+              }}
+              title={title}
+              titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+              meta={[
+                {
+                  name: `description`,
+                  content: metaDescription,
+                },
+                {
+                  property: `og:title`,
+                  content: title,
+                },
+                {
+                  property: `og:description`,
+                  content: metaDescription,
+                },
+                {
+                  property: `og:type`,
+                  content: `website`,
+                },
+                {
+                  name: `twitter:card`,
+                  content: `summary`,
+                },
+                {
+                  name: `twitter:creator`,
+                  content: data.site.siteMetadata.author,
+                },
+                {
+                  name: `twitter:title`,
+                  content: title,
+                },
+                {
+                  name: `twitter:description`,
+                  content: metaDescription,
+                },
+              ].concat(meta)}
+            />
+          )
+        }}
+      />
+    )
+  }
 }
 
 SEO.defaultProps = {
