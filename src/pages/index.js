a9a4844535a6f940c88fb6cb9c965fd1d371e6c9
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-12 23:02:28 +0200

    ♻️ Simplify start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import Image from "../components/image"
 import SEO from "../components/SEO"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hello World!</h1>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
