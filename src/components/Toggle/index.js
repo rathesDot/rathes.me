13ed8f6e8e2eb6635f8181a04f78747781653589
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-13 23:54:29 +0200

    ✨ Make Toggle a button with click event

--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -1,22 +1,29 @@
 import React, { PureComponent } from "react"
+import PropTypes from "prop-types"
 
 class Toggle extends PureComponent {
   render() {
     return (
-      <svg
-        width="30"
-        height="25"
-        viewBox="0 0 30 25"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="cursor-pointer"
-      >
-        <path d="M0 0H30V5H0V0Z" fill="white" />
-        <path d="M0 10H30V15H0V10Z" fill="white" />
-        <path d="M30 20H0V25H30V20Z" fill="white" />
-      </svg>
+      <button onClick={this.props.onClick}>
+        <svg
+          width="30"
+          height="25"
+          viewBox="0 0 30 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="cursor-pointer"
+        >
+          <path d="M0 0H30V5H0V0Z" fill="white" />
+          <path d="M0 10H30V15H0V10Z" fill="white" />
+          <path d="M30 20H0V25H30V20Z" fill="white" />
+        </svg>
+      </button>
     )
   }
+}
+
+Toggle.propTypes = {
+  onClick: PropTypes.func.isRequired,
 }
 
 export default Toggle
