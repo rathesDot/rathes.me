a95af90c9e4f9e3c0121d9ae676a44c2f88e158a
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-13 23:13:22 +0200

    ✨ Introduce basic navigation component --- It is still missing the functionality

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from "react"
+import { Link } from "gatsby"
+
+import Toggle from "../Toggle"
+import Logo from "../Logo"
+
+class Navigation extends PureComponent {
+  render() {
+    return (
+      <div className="mb-24">
+        <div className="flex items-center justify-between mb-4">
+          <Link to="/">
+            <Logo />
+          </Link>
+          <Toggle />
+        </div>
+        <div className="text-right">
+          <Link to="/page-2/" className="font-bold mr-2">
+            <span className="font-normal mr-2">//</span>About
+          </Link>
+          <Link to="/page-2/" className="font-bold mr-2">
+            <span className="font-normal mr-2">//</span>Work
+          </Link>
+          <Link to="/page-2/" className="font-bold mr-2">
+            <span className="font-normal mr-2">//</span>Writings
+          </Link>
+          <Link to="/page-2/" className="font-bold">
+            <span className="font-normal mr-2">//</span>Talks
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navigation
