47a1c23eba9ecb51fa6083065c9d96f16c7240af
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-13 22:41:59 +0200

    💄 Add title styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,10 @@ import SEO from "../components/SEO"
 const IndexPage = () => (
   <Layout className="p-4">
     <SEO title="Home" />
-    <h1>Hello World!</h1>
+    <h1 className="text-5xl font-bold leading-none text-white">
+      Hi there, I’m Rathes Sachchithananthan and I’m a web developer currently
+      living in Belgium
+    </h1>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
