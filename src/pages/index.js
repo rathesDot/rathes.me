ea56a044e88c252fac098c9c38d9eb6085dfd46d
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-19 21:28:18 +0200

    💄 Add OverviewInfo to start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import OverviewInfo from "../components/OverviewInfo"
 
 const IndexPage = () => (
   <Layout className="p-4">
     <SEO title="Home" />
-    <h1 className="text-5xl font-bold leading-none text-white break-words">
+    <h1 className="text-4xl font-bold leading-none text-white break-words xs:text-5xl">
       Hi there, I’m Rathes Sachchithananthan and I’m a web developer currently
       living in Belgium
     </h1>
+    <OverviewInfo />
   </Layout>
 )
 
