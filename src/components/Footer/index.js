0b621b54cbd710a672c58582a0798be7de12922f
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-19 21:47:51 +0200

    💄 Add spacing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 class Footer extends PureComponent {
   render() {
     return (
-      <div className="flex justify-between text-silver-darker">
+      <div className="flex justify-between text-silver-darker mt-4">
         <span className="text-xs">&copy; 2019 — Rathes Sachchithananthan</span>
         <Link className="text-xs" to="/page-2/">
           Legal Information
