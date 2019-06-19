6a6347d769074099b7a3e5d9b29ba0541bf1d072
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-19 21:48:40 +0200

    💄 Add footer to base layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,18 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
+import cx from "classnames"
 
 import Navigation from "../Navigation"
 
 import "./styles.css"
+import Footer from "../Footer"
 
 class Layout extends React.PureComponent {
   render() {
-    const { children, ...props } = this.props
+    const { children, className, ...props } = this.props
 
     return (
-      <main {...props}>
+      <main
+        {...props}
+        className={cx(className, "flex flex-col justify-between min-h-screen")}
+      >
         <Navigation />
         {children}
+        <Footer />
       </main>
     )
   }
