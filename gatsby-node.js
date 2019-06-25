8ea0fac011bc1eea6c2d3ac703da2f010bafd7ae
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-25 22:57:25 +0200

    ✨ Add method to create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const {
@@ -14,4 +15,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: filePath.replace("/articles/", "/blog/"),
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/Blog/index.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
