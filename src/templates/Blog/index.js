86d4fd690be56646865fd289423872befbae81d2
Rathes Sachchithananthan <sachchi@rathes.de>
2019-06-25 23:05:33 +0200

    ✨ Add content to the blog post

--- src/templates/Blog/index.js
+++ src/templates/Blog/index.js
@@ -1,10 +1,28 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../../components/Layout"
 
-export default () => {
+export default ({ data }) => {
+  const post = data.markdownRemark
   return (
-    <Layout>
-      <div>Hello blog post</div>
+    <Layout className="p-4">
+      <div>
+        <h1 className="text-3xl font-bold leading-none text-white break-words xs:text-4xl">
+          {post.frontmatter.title}
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
