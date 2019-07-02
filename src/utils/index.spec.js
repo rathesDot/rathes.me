b77088f7d619384fd23b53d04b9efdd5877440fd
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-02 20:13:39 +0200

    ✅ Ensure it can sort groups

--- src/utils/index.spec.js
+++ src/utils/index.spec.js
@@ -1,4 +1,4 @@
-import { extractBlogPosts, groupPostsByYear } from "./index"
+import { extractBlogPosts, groupPostsByYear, getSortedGroups } from "./index"
 
 describe("utils", () => {
   it("extracts the internal blog posts from graphql data", () => {
@@ -114,5 +114,84 @@ describe("utils", () => {
         },
       ],
     })
+  })
+
+  it("sorts by descending years", () => {
+    const posts = {
+      2017: [
+        {
+          title: "The oldest post",
+          date: "02 June, 2017",
+          link: "/blog/en/the-oldest/",
+        },
+      ],
+      2019: [
+        {
+          title: "The latest posts.",
+          date: "18 July, 2019",
+          link: "/blog/en/customer-experience-instead-of-features/",
+        },
+        {
+          title: "Managing time",
+          date: "08 April, 2019",
+          link: "/blog/en/managing-time/",
+        },
+        {
+          title: "Stop thinking in features.",
+          date: "18 February, 2019",
+          link: "/blog/en/customer-experience-instead-of-features/",
+        },
+      ],
+      2018: [
+        {
+          title: "From Ghent with love",
+          date: "02 June, 2018",
+          link: "/blog/en/from-ghent-with-love/",
+        },
+      ],
+    }
+
+    expect(getSortedGroups(posts)).toEqual([
+      [
+        "2019",
+        [
+          {
+            title: "The latest posts.",
+            date: "18 July, 2019",
+            link: "/blog/en/customer-experience-instead-of-features/",
+          },
+          {
+            title: "Managing time",
+            date: "08 April, 2019",
+            link: "/blog/en/managing-time/",
+          },
+          {
+            title: "Stop thinking in features.",
+            date: "18 February, 2019",
+            link: "/blog/en/customer-experience-instead-of-features/",
+          },
+        ],
+      ],
+      [
+        "2018",
+        [
+          {
+            title: "From Ghent with love",
+            date: "02 June, 2018",
+            link: "/blog/en/from-ghent-with-love/",
+          },
+        ],
+      ],
+      [
+        "2017",
+        [
+          {
+            title: "The oldest post",
+            date: "02 June, 2017",
+            link: "/blog/en/the-oldest/",
+          },
+        ],
+      ],
+    ])
   })
 })
