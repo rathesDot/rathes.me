a13907e1d3e7e6e09ae758b905700874e3f5b3c9
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-02 17:17:42 +0200

    ✨ Introduce a file containing external links

---
+++ src/content/articles/externalLinks.js
@@ -0,0 +1,22 @@
+export default {
+  "Books & Whitepapers": [
+    {
+      title: "A guide to basic Tamil Grammar",
+      link: "https://learn-tamil.com",
+    },
+  ],
+  2018: [
+    {
+      title: "Looking back to 2018",
+      link: "https://medium.com/@rathes/looking-forward-to-2018-927baae0f065",
+    },
+  ],
+  2016: [
+    {
+      title:
+        "Pivot im Leben — Warum ich mich entschied nach Schleswig zu ziehen",
+      link:
+        "https://medium.com/@rathes/pivot-in-leben-warum-ich-mich-entschied-nach-schleswig-zu-ziehen-dbb268db09a6",
+    },
+  ],
+}
