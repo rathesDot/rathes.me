5a093c4a0ba2b38e4e3c0afd3d9f2ba881f5ffc3
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-02 23:08:12 +0200

    ✨ Introduce new page for talks

---
+++ src/pages/talks.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const Talks = () => (
+  <Layout className="p-4">
+    <SEO title="Talks" />
+    <h1 className="text-3xl font-bold leading-none text-white break-words xs:text-4xl">
+      Talks
+    </h1>
+  </Layout>
+)
+
+export default Talks
