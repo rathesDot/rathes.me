f5f7901250ea7c4b23f1cd6ae488a23a361efb82
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-02 20:28:19 +0200

    ♻️ Extract processing the data from template

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -4,64 +4,61 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
-const Writings = ({ data }) => (
-  <Layout className="p-4">
-    <SEO title="Writings" />
-    <h1 className="text-3xl font-bold leading-none text-white break-words xs:text-4xl">
-      My writings
-    </h1>
-    <p>
-      From time to time, I do write. Sometime on my blog, but also on other
-      blogs such as on{" "}
-      <a className="text-white underline" href="https://medium.com/@rathes">
-        Medium
-      </a>
-      . The topics, I write about are diverse. Obviously, I write about the web,
-      but also about non-web topics like my mother tongue Tamil or even about
-      movies or anime.
-    </p>
-    <p>
-      I do write in English, German and Tamil. So if you do not understand the
-      title, it may be that you do not speak the language. If you want me to
-      translate a specific article, just drop a message{" "}
-      <a
-        className="text-white underline"
-        href="https://twitter.com/rswebdesigner"
-      >
-        @rswebdesigner
-      </a>
-    </p>
-    <div className="mt-12">
-      {Object.entries(
-        data.allMarkdownRemark.edges.reduce((acc, { node }) => {
-          ;(acc[new Date(Date.parse(node.frontmatter.date)).getFullYear()] =
-            acc[new Date(Date.parse(node.frontmatter.date)).getFullYear()] ||
-            []).push(node)
-          return acc
-        }, {})
-      )
-        .reverse()
-        .map(([key, nodes]) => {
+import { extractBlogPosts, getSortedGroups, groupPostsByYear } from "../utils"
+
+const Writings = ({ data }) => {
+  const blogPosts = getSortedGroups(groupPostsByYear(extractBlogPosts(data)))
+
+  return (
+    <Layout className="p-4">
+      <SEO title="Writings" />
+      <h1 className="text-3xl font-bold leading-none text-white break-words xs:text-4xl">
+        My writings
+      </h1>
+      <p>
+        From time to time, I do write. Sometime on my blog, but also on other
+        blogs such as on{" "}
+        <a className="text-white underline" href="https://medium.com/@rathes">
+          Medium
+        </a>
+        . The topics, I write about are diverse. Obviously, I write about the
+        web, but also about non-web topics like my mother tongue Tamil or even
+        about movies or anime.
+      </p>
+      <p>
+        I do write in English, German and Tamil. So if you do not understand the
+        title, it may be that you do not speak the language. If you want me to
+        translate a specific article, just drop a message{" "}
+        <a
+          className="text-white underline"
+          href="https://twitter.com/rswebdesigner"
+        >
+          @rswebdesigner
+        </a>
+      </p>
+      <div className="mt-12">
+        {blogPosts.map(([key, posts]) => {
           return (
             <div key={key} className="mb-10">
               <h2 className="text-silver-darker tracking-wider text-sm mb-2">
                 {key}
               </h2>
-              {nodes.map(node => (
+              {posts.map((post, index) => (
                 <Link
-                  to={node.fields.slug}
-                  key={node.id}
+                  to={post.link}
+                  key={index}
                   className="block text-white text-lg mb-2"
                 >
-                  {node.frontmatter.title}
+                  {post.title}
                 </Link>
               ))}
             </div>
           )
         })}
-    </div>
-  </Layout>
-)
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
