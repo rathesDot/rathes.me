1dc3ee09fd17ed73873b0693fd016dd8b7aeb083
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-02 19:44:10 +0200

    ✨ Add function to group by year

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,19 @@ export const extractBlogPosts = data => {
     }
   })
 }
+
+export const groupPostsByYear = posts => {
+  return posts
+    .sort((a, b) =>
+      Date.parse(a.date) < Date.parse(b.date)
+        ? 1
+        : Date.parse(b.date) < Date.parse(a.date)
+        ? -1
+        : 0
+    )
+    .reduce((list, post) => {
+      ;(list[new Date(Date.parse(post.date)).getFullYear()] =
+        list[new Date(Date.parse(post.date)).getFullYear()] || []).push(post)
+      return list
+    }, {})
+}
