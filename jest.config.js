71f338d08dfdd23ad06e28f20fb0bc309aa606be
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-02 18:55:35 +0200

    🔧 Introduce config for jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
+  },
+  testPathIgnorePatterns: [`node_modules`, `.cache`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+}
