c5de86e570b5d87169927ad72207ad58495bedf5
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-03 19:02:08 +0200

    ✨ Introduce a new page for legal info

---
+++ src/pages/legal.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const Legal = ({ data }) => (
+  <Layout className="p-4">
+    <SEO title="Legal Information" />
+    <h1 className="text-3xl font-bold leading-none text-white break-words xs:text-4xl">
+      Legal Information
+    </h1>
+  </Layout>
+)
+
+export default Legal
