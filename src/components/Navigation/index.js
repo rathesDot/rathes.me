8324b49503f83fd387293eb4e855d5aa39311b17
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-28 09:21:48 +0200

    💄 Increase font-size on navigation elements

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -39,29 +39,38 @@ class Navigation extends PureComponent {
             <div
               className={cx("text-right", { hidden: !isNavOpen }, "md:block")}
             >
-              <Link to="/about" className="font-bold mr-2 text-white">
+              <Link
+                to="/about"
+                className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
+              >
                 About
               </Link>
               <a
                 href={data.resume.publicURL}
-                className="font-bold mr-2 text-white"
+                className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
               >
                 <span
-                  className="font-normal text-xl mr-2 text-silver"
+                  className="font-normal text-xl mr-2 text-silver lg:text-3xl"
                   dangerouslySetInnerHTML={{ __html: "&sol;" }}
                 ></span>
                 Resume
               </a>
-              <Link to="/work" className="font-bold mr-2 text-white">
+              <Link
+                to="/work"
+                className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
+              >
                 <span
-                  className="font-normal text-xl mr-2 text-silver"
+                  className="font-normal text-xl mr-2 text-silver lg:text-3xl"
                   dangerouslySetInnerHTML={{ __html: "&sol;" }}
                 ></span>
                 Work
               </Link>
-              <Link to="/writings" className="font-bold text-white">
+              <Link
+                to="/writings"
+                className="font-bold text-white lg:text-2xl lg:leading-none"
+              >
                 <span
-                  className="font-normal text-xl mr-2 text-silver"
+                  className="font-normal text-xl mr-2 text-silver lg:text-3xl"
                   dangerouslySetInnerHTML={{ __html: "&sol;" }}
                 ></span>
                 Writings
