981e84e3222f93d1db1bb0ec7194608aebcd43bf
Rathes Sachchithananthan <sachchi@rathes.de>
2019-07-28 10:02:52 +0200

    💄 Adjust spacing

--- src/components/SayHi/index.js
+++ src/components/SayHi/index.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from "react"
 class SayHi extends PureComponent {
   render() {
     return (
-      <div className="text-4xl xs:text-5xl font-bold text-white max-w-345px leading-none my-8">
+      <div className="text-4xl xs:text-5xl font-bold text-white max-w-345px leading-none mt-16 mb-8">
         Work inquiry, question or something else?{" "}
         <a className="underline" href="mailto:sachchi@rathes.de">
           Say hi!
