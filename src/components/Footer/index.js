df615fa5097d6331152324b93a89969e6392e152
Rathes Sachchithananthan <sachchi@rathes.de>
2019-08-01 14:28:21 +0200

    🐛 Fix footer width not limited on large screens

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 class Footer extends PureComponent {
   render() {
     return (
-      <div className="flex justify-between text-silver-darker mt-4">
+      <div className="flex justify-between text-silver-darker mt-4 lg:max-w-4xl">
         <span className="text-xs">&copy; 2019 — Rathes Sachchithananthan</span>
         <Link className="text-xs" to="/legal">
           Legal Information
