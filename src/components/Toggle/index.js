11cbfb15b622da69427b2574af453c5334c12f51
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-20 17:06:46 +0200

    ♻️ Use functional components over classes

--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -1,28 +1,22 @@
-import React, { PureComponent } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 
-class Toggle extends PureComponent {
-  render() {
-    const { onClick, ...props } = this.props
-
-    return (
-      <button onClick={onClick} {...props}>
-        <svg
-          width="30"
-          height="25"
-          viewBox="0 0 30 25"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="cursor-pointer"
-        >
-          <path d="M0 0H30V5H0V0Z" fill="white" />
-          <path d="M0 10H30V15H0V10Z" fill="white" />
-          <path d="M30 20H0V25H30V20Z" fill="white" />
-        </svg>
-      </button>
-    )
-  }
-}
+const Toggle = ({ onClick, ...props }) => (
+  <button onClick={onClick} {...props}>
+    <svg
+      width="30"
+      height="25"
+      viewBox="0 0 30 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="cursor-pointer"
+    >
+      <path d="M0 0H30V5H0V0Z" fill="white" />
+      <path d="M0 10H30V15H0V10Z" fill="white" />
+      <path d="M30 20H0V25H30V20Z" fill="white" />
+    </svg>
+  </button>
+)
 
 Toggle.propTypes = {
   onClick: PropTypes.func.isRequired,
