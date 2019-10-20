a40e920f34582fcc19a20b77f2c474dd3bdfddc9
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-20 17:03:36 +0200

    ♻️ Use functional component over classes

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -3,78 +3,71 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { graphql, StaticQuery } from "gatsby"
 
-class SEO extends React.PureComponent {
-  render() {
-    const { description, lang, meta, title } = this.props
-
-    return (
-      <StaticQuery
-        query={graphql`
-          query {
-            site {
-              siteMetadata {
-                title
-                description
-                author
-              }
-            }
+const SEO = ({ description, lang, meta, title }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
           }
-        `}
-        render={data => {
-          const metaDescription =
-            description || data.site.siteMetadata.description
-          return (
-            <Helmet
-              htmlAttributes={{
-                lang,
-              }}
-              title={title}
-              titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-              meta={[
-                {
-                  name: `description`,
-                  content: metaDescription,
-                },
-                {
-                  property: `og:title`,
-                  content: title,
-                },
-                {
-                  property: `og:site_name`,
-                  content: `rathes.me`,
-                },
-                {
-                  property: `og:description`,
-                  content: metaDescription,
-                },
-                {
-                  property: `og:type`,
-                  content: `website`,
-                },
-                {
-                  name: `twitter:card`,
-                  content: `summary`,
-                },
-                {
-                  name: `twitter:creator`,
-                  content: data.site.siteMetadata.author,
-                },
-                {
-                  name: `twitter:title`,
-                  content: title,
-                },
-                {
-                  name: `twitter:description`,
-                  content: metaDescription,
-                },
-              ].concat(meta)}
-            />
-          )
-        }}
-      />
-    )
-  }
-}
+        }
+      }
+    `}
+    render={data => {
+      const metaDescription = description || data.site.siteMetadata.description
+      return (
+        <Helmet
+          htmlAttributes={{
+            lang,
+          }}
+          title={title}
+          titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+          meta={[
+            {
+              name: `description`,
+              content: metaDescription,
+            },
+            {
+              property: `og:title`,
+              content: title,
+            },
+            {
+              property: `og:site_name`,
+              content: `rathes.me`,
+            },
+            {
+              property: `og:description`,
+              content: metaDescription,
+            },
+            {
+              property: `og:type`,
+              content: `website`,
+            },
+            {
+              name: `twitter:card`,
+              content: `summary`,
+            },
+            {
+              name: `twitter:creator`,
+              content: data.site.siteMetadata.author,
+            },
+            {
+              name: `twitter:title`,
+              content: title,
+            },
+            {
+              name: `twitter:description`,
+              content: metaDescription,
+            },
+          ].concat(meta)}
+        />
+      )
+    }}
+  />
+)
 
 SEO.defaultProps = {
   lang: `en`,
