fc2fa3c85f7fb68e7e96595d60f2f05a0ae7c079
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-20 16:58:36 +0200

    ♻️ Use hooks to query graphql

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link, StaticQuery, graphql } from "gatsby"
+import { Link, StaticQuery, graphql, useStaticQuery } from "gatsby"
 import cx from "classnames"
 
 import Toggle from "../Toggle"
@@ -7,70 +7,66 @@ import Logo from "../Logo"
 
 const Navigation = () => {
   const [isNavOpen, toggleNavigation] = useState(false)
+  const data = useStaticQuery(graphql`
+    query {
+      resume: file(relativePath: { eq: "files/resume.pdf" }) {
+        publicURL
+      }
+    }
+  `)
 
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          resume: file(relativePath: { eq: "files/resume.pdf" }) {
-            publicURL
-          }
-        }
-      `}
-      render={data => (
-        <div className="mb-20 md:flex md:items-center md:justify-between lg:mt-8 lg:max-w-4xl">
-          <div className="flex items-center justify-between mb-4 md:mb-0">
-            <Link to="/" aria-label="Home">
-              <Logo />
-            </Link>
-            <Toggle
-              className="md:hidden"
-              aria-label="Toggle Navigation"
-              onClick={() => toggleNavigation(!isNavOpen)}
-            />
-          </div>
-          <div className={cx("text-right", { hidden: !isNavOpen }, "md:block")}>
-            <Link
-              to="/about"
-              className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
-            >
-              About
-            </Link>
-            <a
-              href={data.resume.publicURL}
-              className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
-              aria-label="Resume"
-            >
-              <span
-                className="font-normal text-xl mr-2 text-silver lg:text-3xl"
-                dangerouslySetInnerHTML={{ __html: "&sol;" }}
-              ></span>
-              Resume
-            </a>
-            <Link
-              to="/work"
-              className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
-            >
-              <span
-                className="font-normal text-xl mr-2 text-silver lg:text-3xl"
-                dangerouslySetInnerHTML={{ __html: "&sol;" }}
-              ></span>
-              Work
-            </Link>
-            <Link
-              to="/writings"
-              className="font-bold text-white lg:text-2xl lg:leading-none"
-            >
-              <span
-                className="font-normal text-xl mr-2 text-silver lg:text-3xl"
-                dangerouslySetInnerHTML={{ __html: "&sol;" }}
-              ></span>
-              Writings
-            </Link>
-          </div>
-        </div>
-      )}
-    />
+    <div className="mb-20 md:flex md:items-center md:justify-between lg:mt-8 lg:max-w-4xl">
+      <div className="flex items-center justify-between mb-4 md:mb-0">
+        <Link to="/" aria-label="Home">
+          <Logo />
+        </Link>
+        <Toggle
+          className="md:hidden"
+          aria-label="Toggle Navigation"
+          onClick={() => toggleNavigation(!isNavOpen)}
+        />
+      </div>
+      <div className={cx("text-right", { hidden: !isNavOpen }, "md:block")}>
+        <Link
+          to="/about"
+          className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
+        >
+          About
+        </Link>
+        <a
+          href={data.resume.publicURL}
+          className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
+          aria-label="Resume"
+        >
+          <span
+            className="font-normal text-xl mr-2 text-silver lg:text-3xl"
+            dangerouslySetInnerHTML={{ __html: "&sol;" }}
+          ></span>
+          Resume
+        </a>
+        <Link
+          to="/work"
+          className="font-bold mr-2 text-white lg:text-2xl lg:leading-none"
+        >
+          <span
+            className="font-normal text-xl mr-2 text-silver lg:text-3xl"
+            dangerouslySetInnerHTML={{ __html: "&sol;" }}
+          ></span>
+          Work
+        </Link>
+        <Link
+          to="/writings"
+          className="font-bold text-white lg:text-2xl lg:leading-none"
+        >
+          <span
+            className="font-normal text-xl mr-2 text-silver lg:text-3xl"
+            dangerouslySetInnerHTML={{ __html: "&sol;" }}
+          ></span>
+          Writings
+        </Link>
+      </div>
+    </div>
   )
 }
 
