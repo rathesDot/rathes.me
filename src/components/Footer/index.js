c9f563cccedb5002c365ace1cba40395a14aee97
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-20 16:47:58 +0200

    ♻️ Use functional component instead of class

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,13 @@
-import React, { PureComponent } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
-class Footer extends PureComponent {
-  render() {
-    return (
-      <div className="flex justify-between text-silver-darker mt-4 lg:max-w-4xl">
-        <span className="text-xs">&copy; 2019 — Rathes Sachchithananthan</span>
-        <Link className="text-xs" to="/legal">
-          Legal Information
-        </Link>
-      </div>
-    )
-  }
-}
+const Footer = () => (
+  <div className="flex justify-between text-silver-darker mt-4 lg:max-w-4xl">
+    <span className="text-xs">&copy; 2019 — Rathes Sachchithananthan</span>
+    <Link className="text-xs" to="/legal">
+      Legal Information
+    </Link>
+  </div>
+)
 
 export default Footer
