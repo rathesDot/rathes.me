bd6abde0d1bba1481655568b3a7efacae83a353e
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-24 22:26:06 +0200

    🔥 Remove unused dependency

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import cx from "classnames"
 
 import Navigation from "../Navigation"
 import Footer from "../Footer"
@@ -8,21 +7,7 @@ import Footer from "../Footer"
 import "./styles.css"
 
 const Layout = ({ children }) => (
-  <main
-    className={cx(
-      "font-sans",
-      "bg-black",
-      "text-silver",
-      "text-base",
-      "p-4",
-      "md:p-8",
-      "flex",
-      "flex-col",
-      "justify-between",
-      "min-h-screen",
-      "lg:pl-142px"
-    )}
-  >
+  <main className="font-sans bg-black text-silver text-base p-4 md:p-8 flex flex-col justify-between min-h-screen lg:pl-142px">
     <Navigation />
     {children}
     <Footer />
