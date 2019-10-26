7015fc22f13d01c631ba4654c055f9ff61b2e894
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-26 18:42:32 +0200

    ✨ Render MDX posts also in writing list

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -109,6 +109,20 @@ export const query = graphql`
         }
       }
     }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
   }
 `
 
