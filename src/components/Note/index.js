707889ed57946b7e3133acd0ab058640dd9c85f9
Rathes Sachchithananthan <rathes@aheenam.com>
2019-10-28 18:37:33 +0100

    ✨ Introduce a Note component

---
+++ src/components/Note/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Note = ({ element, children }) => {
+  const Element = element || "p"
+
+  return <Element className="bg-black-lighter p-4 -mx-4">{children}</Element>
+}
+
+export default Note
