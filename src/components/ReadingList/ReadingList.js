b5d9f4ef1b9c8846655401284fada7d0e885dda0
Rathes Sachchithananthan <rathes@aheenam.com>
2019-12-13 22:08:42 +0100

    ✨ Render the booklist

--- src/components/ReadingList/ReadingList.js
+++ src/components/ReadingList/ReadingList.js
@@ -1,5 +1,16 @@
 import React from "react"
 
-const ReadingList = () => <ul></ul>
+const ReadingList = ({ children }) => <ul>{children}</ul>
+
+export const Book = ({ author, title, link }) => (
+  <li className="mb-3 leading-normal">
+    <a className="text-white text-lg mb-2 " href={link}>
+      <span className="block text-silver-darker tracking-wider text-sm">
+        {author}
+      </span>
+      {title}
+    </a>
+  </li>
+)
 
 export default ReadingList
