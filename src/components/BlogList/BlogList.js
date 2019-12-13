6d2c419701c5939b7dc58bb131e0c3a9c0546a86
Rathes Sachchithananthan <rathes@aheenam.com>
2019-12-13 22:46:19 +0100

    ✨ Render articles

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,3 +1,11 @@
-const BlogList = () => null
+import React from "react"
+
+const BlogList = ({ children }) => <div>{children}</div>
+
+BlogList.Article = ({ title, link }) => (
+  <a className="block text-white text-lg mb-2" href={link}>
+    {title}
+  </a>
+)
 
 export default BlogList
