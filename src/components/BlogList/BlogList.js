7e8ac4cbe1281443044a1eb750848e62fe716f92
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-06 22:48:12 +0100

    ✨ Pass custom element for link

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -2,10 +2,13 @@ import React from "react"
 
 const BlogList = ({ children }) => <div>{children}</div>
 
-BlogList.Article = ({ title, link }) => (
-  <a className="block text-white text-lg mb-2" href={link}>
-    {title}
-  </a>
-)
+BlogList.Article = ({ title, link, linkElement }) => {
+  const Link = linkElement ?? `a`
+  return (
+    <Link className="block text-white text-lg mb-2" href={link}>
+      {title}
+    </Link>
+  )
+}
 
 export default BlogList
