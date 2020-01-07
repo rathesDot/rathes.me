08557d57735e74d8403aa4a59e4b220884f99548
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-07 19:04:49 +0100

    🔧 Add color config --- This is the same config that is set up via TailwindCSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,21 @@ import React from "react"
 import { ThemeProvider } from "styled-components"
 require("prism-themes/themes/prism-atom-dark.css")
 
-const theme = {}
+const theme = {
+  colors: {
+    black: {
+      lightest: "#454545",
+      lighter: "#252525",
+      default: "#111",
+    },
+    silver: {
+      default: "#CFD3DC",
+      darker: "#CCC",
+      darkest: "#AAA",
+    },
+    white: "#FFF",
+  },
+}
 
 export const wrapRootElement = ({ element }) => {
   return <ThemeProvider theme={theme}>{element}</ThemeProvider>
