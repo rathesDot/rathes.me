bf52dc465140ed4fbde984687806d85fbd49b95a
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-07 19:13:10 +0100

    🔧 Add font related theme config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -16,6 +16,39 @@ const theme = {
     },
     white: "#FFF",
   },
+  fontFamily: {
+    default: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      '"Noto Sans"',
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      '"Noto Color Emoji"',
+    ].concat(" "),
+  },
+  fontSize: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    base: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "4rem",
+  },
+  letterSpacing: {
+    normal: "0",
+    wide: "0.025em",
+    wider: "0.05em",
+  },
 }
 
 export const wrapRootElement = ({ element }) => {
