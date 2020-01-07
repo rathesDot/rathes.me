dbcdad5fcd600a2a9a5613c88d38301ba6e705c7
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-07 20:06:22 +0100

    🐛 Move theme from provider to layout --- Otherwise, pages created on build are not getting the theme property passed down

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,1 @@
-import React from "react"
-import { ThemeProvider } from "styled-components"
-import theme from "./theme"
 require("prism-themes/themes/prism-atom-dark.css")
-
-export const wrapRootElement = ({ element }) => {
-  return <ThemeProvider theme={theme}>{element}</ThemeProvider>
-}
