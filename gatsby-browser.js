9066ceb23fe42ac6a809782bfeea56167c0c6d22
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-07 19:04:19 +0100

    ✨ Wrap app with ThemeProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
+import React from "react"
+import { ThemeProvider } from "styled-components"
 require("prism-themes/themes/prism-atom-dark.css")
+
+const theme = {}
+
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={theme}>{element}</ThemeProvider>
+}
