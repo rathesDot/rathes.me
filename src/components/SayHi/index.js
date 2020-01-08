84c0fa62f7ca6e27b7fd66993ae40ca4581c5143
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-08 22:12:53 +0100

    ♻️ Use Link component instead of a

--- src/components/SayHi/index.js
+++ src/components/SayHi/index.js
@@ -1,15 +1,17 @@
 import React from "react"
 
+import Link from "../Link"
+
 const SayHi = () => (
   <div className="text-4xl xs:text-5xl font-bold text-white max-w-345px leading-none mt-16 mb-8 lg:max-w-480px lg:my-16">
     Work inquiry, question or something else?{" "}
-    <a
+    <Link
       aria-label="Send an E-Mail to hello@rathes.me"
       className="underline"
       href="mailto:hello@rathes.me"
     >
       Say hi!
-    </a>
+    </Link>
   </div>
 )
 
