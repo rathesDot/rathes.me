1543eadc969d58dec9c7349e41df75cf02da8822
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-08 20:51:43 +0100

    💄 Set font size to xs

--- src/components/Typography/TextSmall.js
+++ src/components/Typography/TextSmall.js
@@ -1,7 +1,10 @@
 import React from "react"
+import styled from "styled-components"
 
 const TextSmall = ({ children, element }) => {
-  const Element = element || "span"
+  const Element = styled(element || "span")`
+    font-size: ${props => props.theme.fontSize.xs};
+  `
 
   return <Element>{children}</Element>
 }
