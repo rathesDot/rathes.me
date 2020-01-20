5d9c088db934739479eee6a971018c8a59a1b0a0
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-20 16:56:57 +0100

    💄  Decrease spacing for really small screens

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,10 +8,15 @@ import Link from "../Link"
 
 const Separator = styled.span`
   display: inline-block;
-  margin: 0 4px;
+  margin: 0 8px;
   color: ${props => props.theme.colors.silver.default};
   font-size: ${props => props.theme.fontSize.lg};
   font-family: ${props => props.theme.fontFamily.default};
+
+  @media (max-width: 374px) {
+    margin: 0 4px;
+    font-size: ${props => props.theme.fontSize.base};
+  }
 
   &:before {
     content: "/";
@@ -26,6 +31,10 @@ const MenuItem = styled(Link)`
   font-size: ${props => props.theme.fontSize.lg};
   font-family: ${props => props.theme.fontFamily.default};
   font-weight: 600;
+
+  @media (max-width: 374px) {
+    font-size: ${props => props.theme.fontSize.base};
+  }
 `
 
 const Navigation = () => {
@@ -40,50 +49,48 @@ const Navigation = () => {
   const theme = useTheme()
 
   return (
-    <div className="mb-20 md:flex md:items-center md:justify-between lg:mt-8 lg:max-w-4xl">
-      <div className="flex items-center justify-between mb-4 md:mb-0">
-        <Link element={RouterLink} to="/" aria-label="Home">
-          <Logo
-            color={
-              window.location.pathname === "/"
-                ? theme.colors.white.default
-                : theme.colors.silver.default
-            }
-          />
-        </Link>
-        <Separator />
-        <MenuItem
-          isCurrent={window.location.pathname === "/about"}
-          element={RouterLink}
-          to="/about"
-        >
-          About
-        </MenuItem>
-        <Separator />
-        <MenuItem
-          isCurrent={window.location.pathname === "/resume"}
-          href={data.resume.publicURL}
-          aria-label="Resume"
-        >
-          Resume
-        </MenuItem>
-        <Separator />
-        <MenuItem
-          isCurrent={window.location.pathname === "/work"}
-          element={RouterLink}
-          to="/work"
-        >
-          Work
-        </MenuItem>
-        <Separator />
-        <MenuItem
-          isCurrent={window.location.pathname === "/writings"}
-          element={RouterLink}
-          to="/writings"
-        >
-          Writings
-        </MenuItem>
-      </div>
+    <div className="mb-20 flex items-center md:justify-between lg:mt-8 lg:max-w-4xl">
+      <Link element={RouterLink} to="/" aria-label="Home">
+        <Logo
+          color={
+            window.location.pathname === "/"
+              ? theme.colors.white.default
+              : theme.colors.silver.default
+          }
+        />
+      </Link>
+      <Separator />
+      <MenuItem
+        isCurrent={window.location.pathname === "/about"}
+        element={RouterLink}
+        to="/about"
+      >
+        About
+      </MenuItem>
+      <Separator />
+      <MenuItem
+        isCurrent={window.location.pathname === "/resume"}
+        href={data.resume.publicURL}
+        aria-label="Resume"
+      >
+        Resume
+      </MenuItem>
+      <Separator />
+      <MenuItem
+        isCurrent={window.location.pathname === "/work"}
+        element={RouterLink}
+        to="/work"
+      >
+        Work
+      </MenuItem>
+      <Separator />
+      <MenuItem
+        isCurrent={window.location.pathname === "/writings"}
+        element={RouterLink}
+        to="/writings"
+      >
+        Writings
+      </MenuItem>
     </div>
   )
 }
