3879a68f8661b492d5bfcaf67a30e9df767679eb
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-20 17:42:29 +0100

    ✨  Introduce Separator component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import TextSmall from "../Typography/TextSmall"
+
+const Separator = styled.span`
+  display: inline-block;
+  margin: 0 8px;
+  color: ${props => props.theme.colors.silver.default};
+  font-size: ${props => props.theme.fontSize.xs};
+  font-family: ${props => props.theme.fontFamily.default};
+
+  &:before {
+    content: "/";
+  }
+`
 
 const Footer = () => (
   <div className="flex justify-between text-silver-darker mt-4 lg:max-w-4xl">
