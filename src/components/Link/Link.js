4f4299e7b7d60bb12d0784666e02f5f58dd5a019
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-20 16:48:55 +0100

    💄  Highlight active link

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,7 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 
-const Link = ({ children, element, color, tint = "default", ...props }) => {
+const Link = ({
+  children,
+  element,
+  color,
+  tint = "default",
+  isCurrent,
+  ...props
+}) => {
   const Element = styled(element || "a")`
     color: ${props => (color ? props.theme.colors[color][tint] : undefined)};
   `
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link as RouterLink, graphql, useStaticQuery } from "gatsby"
-import styled from "styled-components"
+import styled, { useTheme } from "styled-components"
 
 import Logo from "../Logo"
 
@@ -20,7 +20,7 @@ const Separator = styled.span`
 
 const MenuItem = styled(Link)`
   color: ${props =>
-    props.isActive
+    props.isCurrent
       ? props.theme.colors.white.default
       : props.theme.colors.silver.default};
   font-size: ${props => props.theme.fontSize.lg};
@@ -37,26 +37,50 @@ const Navigation = () => {
     }
   `)
 
+  const theme = useTheme()
+
   return (
     <div className="mb-20 md:flex md:items-center md:justify-between lg:mt-8 lg:max-w-4xl">
       <div className="flex items-center justify-between mb-4 md:mb-0">
         <Link element={RouterLink} to="/" aria-label="Home">
-          <Logo />
+          <Logo
+            color={
+              window.location.pathname === "/"
+                ? theme.colors.white.default
+                : theme.colors.silver.default
+            }
+          />
         </Link>
         <Separator />
-        <MenuItem element={RouterLink} to="/about">
+        <MenuItem
+          isCurrent={window.location.pathname === "/about"}
+          element={RouterLink}
+          to="/about"
+        >
           About
         </MenuItem>
         <Separator />
-        <MenuItem href={data.resume.publicURL} aria-label="Resume">
+        <MenuItem
+          isCurrent={window.location.pathname === "/resume"}
+          href={data.resume.publicURL}
+          aria-label="Resume"
+        >
           Resume
         </MenuItem>
         <Separator />
-        <MenuItem element={RouterLink} to="/work">
+        <MenuItem
+          isCurrent={window.location.pathname === "/work"}
+          element={RouterLink}
+          to="/work"
+        >
           Work
         </MenuItem>
         <Separator />
-        <MenuItem element={RouterLink} to="/writings">
+        <MenuItem
+          isCurrent={window.location.pathname === "/writings"}
+          element={RouterLink}
+          to="/writings"
+        >
           Writings
         </MenuItem>
       </div>
