7aff08fd654df3ec15950c7211e0dd0ab3a20c32
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-20 17:54:22 +0100

    ✨  Introduce a main container component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 
 import Navigation from "../Navigation"
 import Footer from "../Footer"
@@ -8,6 +8,25 @@ import Footer from "../Footer"
 import theme from "../../../theme"
 
 import "./styles.css"
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: ${props => props.theme.colors.black.default};
+  color: ${props => props.theme.colors.silver.default};
+  font-family: ${props => props.theme.fontFamily.default};
+  min-height: 100vh;
+  padding: ${props => props.theme.spacing["4"]};
+
+  @media (min-width: 768px) {
+    padding: 60px 0 32px 60px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 110px 0 32px 110px;
+  }
+`
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
