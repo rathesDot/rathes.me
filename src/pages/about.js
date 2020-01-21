b2cea11b10a91956adcf1f2b5bee7f1ab75d7cde
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:34:02 +0100

    ♻️  Replace p with Paragraph

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import SEO from "../components/SEO"
 import SayHi from "../components/SayHi"
 import ReadingList from "../components/ReadingList"
 import styled from "styled-components"
-import { Heading1 } from "../components/Typography"
+import { Heading1, Paragraph } from "../components/Typography"
 
 const Container = styled.section`
   flex-grow: 1;
@@ -18,57 +18,57 @@ const AboutMe = () => (
     <SEO title="About me" />
     <Container>
       <Heading1>/raðiːs/</Heading1>
-      <p>
+      <Paragraph>
         I’m Rathes Sachchithananthan, a web developer with not only interests in
         engineering but also product development & design, user & customer
         experience and most importantly in languages and cultures.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         I started working as a web designer and web developer at very young
         ages. At the age of 14, I earned my first money as a web designer
         creating custom MySpace layouts and in 2008, at the age of 16, I founded
         my first own company for web design and development. Since then I have
         worked with several clients doing quite a wide range of tasks.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         For more than 10 years I have been working as a web developer doing
         solving problems as a back-end engineer using languages such as PHP,
         Python or Node.js but also as a front-end developer working with
         HTML/CSS and JavaScript.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         However, my real passion is not for software and development. I’m more
         into solving problems for people, make their lives easier and connect
         them with each other. Together with that, I’m passionate about learning
         new languages and cultures.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         Nowadays, I work as a web developer at Teamleader in Ghent, Belgium
         building things using PHP in the back-end and React in the front-end
         with the best team in the world, team ONE.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         Apart from that, I spent most of my time working smaller side projects
         at Aheenam. Right now I’m working on providing a platform for people to
         learn my mother language Tamil.
-      </p>
+      </Paragraph>
 
       <h2 className="text-3xl mt-12 font-bold leading-none text-white break-words xs:text-4xl">
         What I read
       </h2>
-      <p>
+      <Paragraph>
         Books play an important part in my life. Just sitting my couch or bed
         and reading a novel can be really inspiring.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         I do fancy crime novels with interesting detective characters from good
         old Sherlock Holmes to Harry Hole. But I read basically any kind of
         books that can broaden one’s horizons.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         Actually, I dream to have my own library one where I can invite
         everybody to pick up a nice book and read.
-      </p>
+      </Paragraph>
       <h3 className="text-xl mt-12 font-bold leading-none text-white break-words xs:text-2xl">
         Reading List 2020
       </h3>
@@ -95,21 +95,21 @@ const AboutMe = () => (
       <h2 className="text-3xl mt-12 font-bold leading-none text-white break-words xs:text-4xl">
         What I write
       </h2>
-      <p>
+      <Paragraph>
         From time to time I also write. It can be work related topics that I
         share in the writing section of this website but I also write to empty
         my mind and eternalize ideas.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         I have several ideas for some crime novel and I even developed my
         detective character “Chris Garner”. I want to publish my first short
         stories soon.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         Before I can do that I have to finish my current book which is called “A
         guide to basic Tamil Grammar”. I’m writing that book as an entry point
         for my learning platform.
-      </p>
+      </Paragraph>
       <SayHi />
     </Container>
   </Layout>
