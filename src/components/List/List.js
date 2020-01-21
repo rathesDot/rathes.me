8c580608da68934ffddc34174f857bc6b84810dd
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 09:20:16 +0100

    ✨  Render children after title

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,16 @@
 import React from "react"
+
 import { Heading4 } from "../Typography"
+import { Box } from "../Box"
 
 const Item = () => null
 
-const List = ({ title }) => <Heading4>{title}</Heading4>
+const List = ({ title, children }) => (
+  <Box>
+    <Heading4>{title}</Heading4>
+    {children}
+  </Box>
+)
 
 List.Item = Item
 
