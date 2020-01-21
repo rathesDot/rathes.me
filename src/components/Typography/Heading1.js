747a3d8b02791f102bf0e9108e676072c0b69709
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 17:58:14 +0100

    💄  Decrease line height of Heading

--- src/components/Typography/Heading1.js
+++ src/components/Typography/Heading1.js
@@ -5,7 +5,7 @@ const Heading1 = ({ children, element, color, tint = "default", ...props }) => {
   const Element = styled(element || "h1")`
     font-size: ${props => props.theme.fontSize["2xl"]};
     font-weight: 600;
-    line-height: 2;
+    line-height: 1.35;
     color: ${props =>
       color
         ? props.theme.colors[color][tint]
