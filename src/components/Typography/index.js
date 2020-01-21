d9bdc2bb827308a3af2d5a7f3944d34264cfcb7f
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:59:13 +0100

    ✨  Expose Heading4

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -2,6 +2,7 @@ import TextSmall from "./TextSmall"
 import TextBody from "./TextBody"
 import Heading1 from "./Heading1"
 import Heading2 from "./Heading2"
+import Heading4 from "./Heading4"
 import Paragraph from "./Paragraph"
 
-export { TextSmall, TextBody, Heading1, Heading2, Paragraph }
+export { TextSmall, TextBody, Heading1, Heading2, Heading4, Paragraph }
