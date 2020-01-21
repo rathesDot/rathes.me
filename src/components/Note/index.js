dc4d94ea7c2c3fd5c0f26f4396825614239c50cd
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 17:14:09 +0100

    🎨  Return directly

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -2,22 +2,20 @@ import React from "react"
 
 import { Box } from "../Box"
 
-const Note = ({ element, children }) => {
-  return (
-    <Box
-      element={element || "p"}
-      backgroundColor="black"
-      backgroundTint="light"
-      marginTop={8}
-      marginBottom={8}
-      padding={4}
-      textColor="silver"
-      textTint="darker"
-      fontSize="sm"
-    >
-      {children}
-    </Box>
-  )
-}
+const Note = ({ element, children }) => (
+  <Box
+    element={element || "p"}
+    backgroundColor="black"
+    backgroundTint="light"
+    marginTop={8}
+    marginBottom={8}
+    padding={4}
+    textColor="silver"
+    textTint="darker"
+    fontSize="sm"
+  >
+    {children}
+  </Box>
+)
 
 export default Note
