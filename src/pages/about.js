afb35967ed798605c338ca0bfb76625cb01bac89
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:50:38 +0100

    💄  Separate paragraphs in sections

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import SayHi from "../components/SayHi"
 import ReadingList from "../components/ReadingList"
 import styled from "styled-components"
 import { Heading1, Paragraph, Heading2 } from "../components/Typography"
+import { Box } from "../components/Box"
 
 const Container = styled.section`
   flex-grow: 1;
@@ -53,60 +54,67 @@ const AboutMe = () => (
         learn my mother language Tamil.
       </Paragraph>
 
-      <Heading2>What I read</Heading2>
-      <Paragraph>
-        Books play an important part in my life. Just sitting my couch or bed
-        and reading a novel can be really inspiring.
-      </Paragraph>
-      <Paragraph>
-        I do fancy crime novels with interesting detective characters from good
-        old Sherlock Holmes to Harry Hole. But I read basically any kind of
-        books that can broaden one’s horizons.
-      </Paragraph>
-      <Paragraph>
-        Actually, I dream to have my own library one where I can invite
-        everybody to pick up a nice book and read.
-      </Paragraph>
-      <h3 className="text-xl mt-12 font-bold leading-none text-white break-words xs:text-2xl">
-        Reading List 2020
-      </h3>
-      <ReadingList>
-        <ReadingList.Book
-          author="Cody McFadyen"
-          link="https://amzn.to/2tw7Zxp"
-          title="Shadow Man (Smoky Barrett, Band 1)"
-        />
-      </ReadingList>
-      <hr className="my-8 block w-10 border-t border-silver-darkest" />
-      <ReadingList>
-        <ReadingList.Book
-          author="Ransom Riggs"
-          link="https://amzn.to/2MYAfj6"
-          title="Library of Souls: The Third Novel of Miss Peregrine's Peculiar Children (Currently Reading)"
-        />
-        <ReadingList.Book
-          author="George R. R. Martin"
-          link="https://amzn.to/2Rngm82"
-          title="A Game of Thrones: A Song of Ice and Fire (Currently Reading)"
-        />
-      </ReadingList>
-      <Heading2>What I write</Heading2>
-      <Paragraph>
-        From time to time I also write. It can be work related topics that I
-        share in the writing section of this website but I also write to empty
-        my mind and eternalize ideas.
-      </Paragraph>
-      <Paragraph>
-        I have several ideas for some crime novel and I even developed my
-        detective character “Chris Garner”. I want to publish my first short
-        stories soon.
-      </Paragraph>
-      <Paragraph>
-        Before I can do that I have to finish my current book which is called “A
-        guide to basic Tamil Grammar”. I’m writing that book as an entry point
-        for my learning platform.
-      </Paragraph>
-      <SayHi />
+      <Box marginTop={32} element="section">
+        <Heading2>What I read</Heading2>
+        <Paragraph>
+          Books play an important part in my life. Just sitting my couch or bed
+          and reading a novel can be really inspiring.
+        </Paragraph>
+        <Paragraph>
+          I do fancy crime novels with interesting detective characters from
+          good old Sherlock Holmes to Harry Hole. But I read basically any kind
+          of books that can broaden one’s horizons.
+        </Paragraph>
+        <Paragraph>
+          Actually, I dream to have my own library one where I can invite
+          everybody to pick up a nice book and read.
+        </Paragraph>
+        <h3 className="text-xl mt-12 font-bold leading-none text-white break-words xs:text-2xl">
+          Reading List 2020
+        </h3>
+        <ReadingList>
+          <ReadingList.Book
+            author="Cody McFadyen"
+            link="https://amzn.to/2tw7Zxp"
+            title="Shadow Man (Smoky Barrett, Band 1)"
+          />
+        </ReadingList>
+        <hr className="my-8 block w-10 border-t border-silver-darkest" />
+        <ReadingList>
+          <ReadingList.Book
+            author="Ransom Riggs"
+            link="https://amzn.to/2MYAfj6"
+            title="Library of Souls: The Third Novel of Miss Peregrine's Peculiar Children (Currently Reading)"
+          />
+          <ReadingList.Book
+            author="George R. R. Martin"
+            link="https://amzn.to/2Rngm82"
+            title="A Game of Thrones: A Song of Ice and Fire (Currently Reading)"
+          />
+        </ReadingList>
+      </Box>
+
+      <Box marginTop={32} element="section">
+        <Heading2>What I write</Heading2>
+        <Paragraph>
+          From time to time I also write. It can be work related topics that I
+          share in the writing section of this website but I also write to empty
+          my mind and eternalize ideas.
+        </Paragraph>
+        <Paragraph>
+          I have several ideas for some crime novel and I even developed my
+          detective character “Chris Garner”. I want to publish my first short
+          stories soon.
+        </Paragraph>
+        <Paragraph>
+          Before I can do that I have to finish my current book which is called
+          “A guide to basic Tamil Grammar”. I’m writing that book as an entry
+          point for my learning platform.
+        </Paragraph>
+      </Box>
+      <Box marginTop={32}>
+        <SayHi />
+      </Box>
     </Container>
   </Layout>
 )
