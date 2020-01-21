f8f8f39a53e09ddbd2258de609f7ae027ff94256
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 10:52:55 +0100

    ✨  Add margin bottom prop

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,8 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 
-const Box = ({ children, element, marginTop, ...props }) => {
+const Box = ({ children, element, marginTop, marginBottom, ...props }) => {
   const Element = styled(element || "div")`
+    margin-bottom: ${props => props.theme.spacing[marginBottom]};
     margin-top: ${props => props.theme.spacing[marginTop]};
   `
 
