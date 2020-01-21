360baeb37a16a8a1b2928ad411b84264f987b247
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 15:49:57 +0100

    🔧  Map components to markup

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,16 @@
-require("prism-themes/themes/prism-atom-dark.css")
+import React from "react"
+
+import { MDXProvider } from "@mdx-js/react"
+import { Heading1, Heading2, Paragraph } from "./src/components/Typography"
+
+import "prism-themes/themes/prism-atom-dark.css"
+
+const components = {
+  h1: Heading1,
+  h2: Heading2,
+  p: Paragraph,
+}
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+)
