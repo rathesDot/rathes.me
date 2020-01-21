91f6cf0127ffef1c1b96f1486729b0ab3975c308
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:48:32 +0100

    ✨  Introduce marginTop property

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,8 +1,10 @@
 import React from "react"
 import styled from "styled-components"
 
-const Box = ({ children, element }) => {
-  const Element = styled(element || "div")
+const Box = ({ children, element, marginTop }) => {
+  const Element = styled(element || "div")`
+    margin-top: ${props => props.theme.spacing[marginTop]};
+  `
 
   return <Element>{children}</Element>
 }
