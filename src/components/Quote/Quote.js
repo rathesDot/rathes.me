9b0898ba86e4ca5c56cc8ab5ec36fd0c05b3a7a8
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 18:10:43 +0100

    ✨  Implement Quote

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,3 +1,32 @@
-const Quote = () => null
+import React from "react"
+
+import { Box } from "../Box"
+import styled from "styled-components"
+
+const Quote = ({ children }) => {
+  const QuoteContainer = styled(Box)`
+    p {
+      margin: 0;
+      color: ${props => props.theme.colors.white.default};
+      font-size: ${props => props.theme.fontSize["2xl"]};
+      line-height: 1.35;
+    }
+  `
+
+  return (
+    <QuoteContainer
+      element="blockquote"
+      backgroundColor="black"
+      backgroundTint="light"
+      marginTop={8}
+      marginBottom={8}
+      padding={4}
+      textColor="white"
+      fontSize="2xl"
+    >
+      {children}
+    </QuoteContainer>
+  )
+}
 
 export default Quote
