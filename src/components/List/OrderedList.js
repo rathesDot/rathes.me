04e8fcf4287662c1e5da416dfdaa544a9c3aa0fe
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 17:52:49 +0100

    💄  Render a OrderedList properly

--- src/components/List/OrderedList.js
+++ src/components/List/OrderedList.js
@@ -1,3 +1,36 @@
-const OrderedList = () => null
+import React from "react"
+import styled from "styled-components"
+
+import { Box } from "../Box"
+
+const OrderedListWrapper = styled(Box)`
+  list-style-type: decimal;
+
+  > li:not(:last-child) {
+    margin: 0 0 ${props => props.theme.spacing[4]} 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`
+
+const OrderedList = ({ children, ...props }) => (
+  <OrderedListWrapper
+    element="ol"
+    backgroundColor="black"
+    backgroundTint="light"
+    marginTop={8}
+    marginBottom={8}
+    padding={6}
+    paddingLeft={8}
+    textColor="silver"
+    textTint="darker"
+    fontSize="sm"
+    {...props}
+  >
+    {children}
+  </OrderedListWrapper>
+)
 
 export default OrderedList
