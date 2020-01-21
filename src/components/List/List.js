48d3d7a2559bbd25057d41aca5c9109aa0b761a5
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 09:29:48 +0100

    ✨  Add item content as body text

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,15 @@
 import React from "react"
 
-import { Heading4 } from "../Typography"
+import { Heading4, TextBody } from "../Typography"
 import { Box } from "../Box"
 
-const Item = () => null
+const Item = ({ children }) => (
+  <Box>
+    <TextBody element="p" color="white">
+      {children}
+    </TextBody>
+  </Box>
+)
 
 const List = ({ title, children, ...props }) => (
   <Box {...props}>
