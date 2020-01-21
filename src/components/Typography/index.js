02ab412306c3a820b9108aaa83c1f68fb9b8143a
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:33:51 +0100

    ✨  Expose Paragraph

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -1,5 +1,6 @@
 import TextSmall from "./TextSmall"
 import TextBody from "./TextBody"
 import Heading1 from "./Heading1"
+import Paragraph from "./Paragraph"
 
-export { TextSmall, TextBody, Heading1 }
+export { TextSmall, TextBody, Heading1, Paragraph }
