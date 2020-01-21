4b5deecd1d2a8ea4866d1796b76b317e530e3e86
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 11:23:28 +0100

    ✨  Add fontFamily prop

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -4,12 +4,17 @@ import styled from "styled-components"
 const Box = ({
   children,
   element,
+  fontFamily,
   marginTop,
   marginBottom,
   maxWidth,
   ...props
 }) => {
   const Element = styled(element || "div")`
+    ${props =>
+      fontFamily
+        ? `font-family: ${props.theme.fontFamily[fontFamily]};`
+        : undefined}
     margin-bottom: ${props => props.theme.spacing[marginBottom]};
     margin-top: ${props => props.theme.spacing[marginTop]};
     ${maxWidth ? `max-width: ${maxWidth}` : undefined};
