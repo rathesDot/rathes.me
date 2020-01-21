a73b5dbef29476067949aab49a05b0042ef75e4d
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:43:03 +0100

    ✨  Render custom element

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,3 +1,10 @@
-const Box = ({ children }) => children
+import React from "react"
+import styled from "styled-components"
+
+const Box = ({ children, element }) => {
+  const Element = styled(element || "div")
+
+  return <Element>{children}</Element>
+}
 
 export default Box
