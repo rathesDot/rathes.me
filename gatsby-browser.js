69aa9c631540019188461de9fb7c465b58bab747
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 15:53:50 +0100

    🔧  Map Heading3

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,21 @@
 import React from "react"
 
 import { MDXProvider } from "@mdx-js/react"
-import { Heading1, Heading2, Paragraph } from "./src/components/Typography"
+import {
+  Heading1,
+  Heading2,
+  Paragraph,
+  Heading3,
+  Heading4,
+} from "./src/components/Typography"
 
 import "prism-themes/themes/prism-atom-dark.css"
 
 const components = {
   h1: Heading1,
   h2: Heading2,
+  h3: Heading3,
+  h4: Heading4,
   p: Paragraph,
 }
 
