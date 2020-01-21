e52d0f21161f8f276a3896b4daf60228dc9abb03
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 09:18:24 +0100

    ✨  Introduce list item component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { Heading4 } from "../Typography"
 
+const Item = () => null
+
 const List = ({ title }) => <Heading4>{title}</Heading4>
+
+List.Item = Item
 
 export default List
