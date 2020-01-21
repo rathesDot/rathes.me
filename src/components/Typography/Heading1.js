467326275dd7c8ac8b466c73e8c35c3f6e712a2e
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:22:34 +0100

    ✨  Introduce Heading1 component

---
+++ src/components/Typography/Heading1.js
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+
+const Heading1 = ({ children, element, color, tint = "default", ...props }) => {
+  const Element = styled(element || "h1")`
+    font-size: ${props => props.theme.fontSize["2xl"]};
+    font-weight: 600;
+    line-height: 2;
+    color: ${props =>
+      color
+        ? props.theme.colors[color][tint]
+        : props.theme.colors.white.default};
+  `
+
+  return <Element {...props}>{children}</Element>
+}
+
+export default Heading1
