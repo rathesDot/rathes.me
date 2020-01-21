ed1aa7a2826afb20af0d3a143a93a10571108d31
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 08:59:03 +0100

    ✨  Introduce new Heading4 component --- Heading3 will be added later on since it is not needed now

---
+++ src/components/Typography/Heading4.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+
+const Heading4 = ({ children, element, color, tint = "default", ...props }) => {
+  const Element = styled(element || "h4")`
+    font-size: ${props => props.theme.fontSize.xs};
+    font-weight: 400;
+    line-height: 2.5;
+    text-transform: uppercase;
+    letter-spacing: ${props => props.theme.letterSpacing.widest};
+    color: ${props =>
+      color
+        ? props.theme.colors[color][tint]
+        : props.theme.colors.silver.default};
+  `
+
+  return <Element {...props}>{children}</Element>
+}
+
+export default Heading4
