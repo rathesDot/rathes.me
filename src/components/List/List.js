405977f5adb369803845db8b6793101f6c248f83
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 09:41:25 +0100

    ✨  Add subtitle

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,22 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Heading4, TextBody } from "../Typography"
+import { Heading4, TextBody, TextSmall } from "../Typography"
 import { Box } from "../Box"
 
-const Item = ({ children }) => {
+const Item = ({ children, subtitle }) => {
   const Title = styled(TextBody)`
     margin: ${props => props.theme.spacing[2]} 0;
+  `
+
+  const Subtitle = styled(TextSmall)`
+    display: block;
   `
 
   return (
     <Title element="li" color="white">
       {children}
+      <Subtitle color="silver">{subtitle}</Subtitle>
     </Title>
   )
 }
