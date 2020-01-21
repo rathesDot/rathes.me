8f2aa3cb32d3428ac9dcbe10415c5c9a0622a102
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 11:12:14 +0100

    ✨  Add maxWidth prop

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,10 +1,18 @@
 import React from "react"
 import styled from "styled-components"
 
-const Box = ({ children, element, marginTop, marginBottom, ...props }) => {
+const Box = ({
+  children,
+  element,
+  marginTop,
+  marginBottom,
+  maxWidth,
+  ...props
+}) => {
   const Element = styled(element || "div")`
     margin-bottom: ${props => props.theme.spacing[marginBottom]};
     margin-top: ${props => props.theme.spacing[marginTop]};
+    ${maxWidth ? `max-width: ${maxWidth}` : undefined};
   `
 
   return <Element {...props}>{children}</Element>
