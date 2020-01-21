8b9a927c1a3b9297d61b3240256dd55c679db2e6
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 15:20:50 +0100

    ♻️  Use components instead of html

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,48 +6,50 @@ import SEO from "../components/SEO"
 import Link from "../components/Link"
 
 import { Link as RouterLink } from "gatsby"
+import { Box } from "../components/Box"
+import { Heading1, Paragraph } from "../components/Typography"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <section className="flex flex-col justify-center flex-grow">
-      <section className="md:max-w-570px md:text-lg">
-        <h1 className="text-3xl font-bold leading-none text-white break-words xs:text-4xl">
-          Looks like you are looking for a page that does not exist.
-        </h1>
-        <p>Here are some things that you might have been looking for:</p>
-        <p>
-          <Link
-            element={RouterLink}
-            color="white"
-            to="/"
-            className="block text-lg mb-2"
-          >
-            The homepage
-          </Link>
-          <Link
-            element={RouterLink}
-            color="white"
-            to="/writings"
-            className="block text-lg mb-2"
-          >
-            Things I write about
-          </Link>
-          <Link
-            element={RouterLink}
-            color="white"
-            to="/about"
-            className="block text-lg mb-2"
-          >
-            Information about myself
-          </Link>
-        </p>
-        <p>
-          Or if you came here from a Google Search, feel free to go back and try
-          a different search term.
-        </p>
-      </section>
-    </section>
+    <Box maxWidth="640px">
+      <Heading1>
+        Looks like you are looking for a page that does not exist.
+      </Heading1>
+      <Paragraph>
+        Here are some things that you might have been looking for:
+      </Paragraph>
+      <p>
+        <Link
+          element={RouterLink}
+          color="white"
+          to="/"
+          className="block text-lg mb-2"
+        >
+          The homepage
+        </Link>
+        <Link
+          element={RouterLink}
+          color="white"
+          to="/writings"
+          className="block text-lg mb-2"
+        >
+          Things I write about
+        </Link>
+        <Link
+          element={RouterLink}
+          color="white"
+          to="/about"
+          className="block text-lg mb-2"
+        >
+          Information about myself
+        </Link>
+      </p>
+      <Paragraph>
+        Or if you came here from a Google Search, feel free to go back and try a
+        different search term.
+      </Paragraph>
+    </Box>
   </Layout>
 )
 
