056b68fc5d8d7d287dbb569d73600a0b078d78c9
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-21 10:54:18 +0100

    ♻️  Replace Tailwind with Styled components

--- src/components/SayHi/index.js
+++ src/components/SayHi/index.js
@@ -1,18 +1,29 @@
 import React from "react"
+import styled from "styled-components"
 
 import Link from "../Link"
+import { Box } from "../Box"
+
+const Container = styled(Box)`
+  font-family: ${props => props.theme.fontFamily.default};
+  font-size: ${props => props.theme.fontSize["4xl"]};
+  color: ${props => props.theme.colors.white.default};
+  font-weight: 600;
+  max-width: 520px;
+  line-height: 1.194444444444;
+`
 
 const SayHi = () => (
-  <div className="text-4xl xs:text-5xl font-bold text-white max-w-345px leading-none mt-16 mb-8 lg:max-w-480px lg:my-16">
+  <Container>
     Work inquiry, question or something else?{" "}
     <Link
       aria-label="Send an E-Mail to hello@rathes.me"
-      className="underline"
       href="mailto:hello@rathes.me"
+      underlined
     >
       Say hi!
     </Link>
-  </div>
+  </Container>
 )
 
 export default SayHi
