beb7004f9949cde1e1ed5ef4a7fcc42229764ffe
Rathes Sachchithananthan <rathes@aheenam.com>
2020-01-31 13:31:41 +0100

    🔥  Remove unused color prop

--- src/components/Separator/Separator.js
+++ src/components/Separator/Separator.js
@@ -3,7 +3,6 @@ import styled from "styled-components"
 const Separator = styled.hr`
   border-width: 2px;
   border-color: ${props => props.theme.colors.black.lighter};
-  color: ${props => props.theme.colors.black.lighter};
   width: 24px;
   margin: ${props => props.theme.spacing[8]} 0;
 `
