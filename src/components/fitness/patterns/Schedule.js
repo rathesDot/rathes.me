ee764857e8472603e43fa85ab7be64ffc8564a16
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-14 19:37:24 +0200

    ✨ Introduce Schedule entry component

--- src/components/fitness/patterns/Schedule.js
+++ src/components/fitness/patterns/Schedule.js
@@ -1,13 +1,27 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Title } from "../index"
+import { BodyText, Heading2, Title } from "../index"
 
 const Schedule = ({ title, children }) => (
   <>
     <Title>{title}</Title>
     {children}
   </>
+)
+
+const Entry = styled.button`
+  color: ${(props) => (props.active ? "#FAF089" : "#FFF")};
+  display: block;
+  margin-top: 29px;
+  text-align: left;
+`
+
+Schedule.Entry = ({ title, date, onClick }) => (
+  <Entry onClick={onClick}>
+    <Heading2>{title}</Heading2>
+    <BodyText>{date}</BodyText>
+  </Entry>
 )
 
 export { Schedule }
