a19c382707764e855925f9cfbe6419ef3872d751
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-14 16:19:30 +0200

    ✨ Introduce minimal Heading2 component

--- src/components/fitness/index.js
+++ src/components/fitness/index.js
@@ -1,4 +1,5 @@
 import { Layout } from "./Layout"
 import { Title } from "./typography/Title"
+import { Heading2 } from "./typography/Heading2"
 
-export { Layout, Title }
+export { Layout, Title, Heading2 }
