ad6261368fb2c451aabd19233d4974e776533fe3
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-14 17:27:23 +0200

    ✨ Introduce Heading3 component

--- src/components/fitness/index.js
+++ src/components/fitness/index.js
@@ -2,6 +2,7 @@ import { Layout } from "./Layout"
 import { Title } from "./typography/Title"
 import { Heading2 } from "./typography/Heading2"
 import { Heading1 } from "./typography/Heading1"
+import { Heading3 } from "./typography/Heading3"
 import { BodyText } from "./typography/BodyText"
 
-export { BodyText, Layout, Title, Heading1, Heading2 }
+export { BodyText, Layout, Title, Heading1, Heading2, Heading3 }
