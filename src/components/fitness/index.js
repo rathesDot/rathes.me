1511f36a7ac6f35fab413ddb18e469608cbc2c85
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-14 16:24:29 +0200

    ✨ Add BodyText component

--- src/components/fitness/index.js
+++ src/components/fitness/index.js
@@ -1,5 +1,6 @@
 import { Layout } from "./Layout"
 import { Title } from "./typography/Title"
 import { Heading2 } from "./typography/Heading2"
+import { BodyText } from "./typography/BodyText"
 
-export { Layout, Title, Heading2 }
+export { BodyText, Layout, Title, Heading2 }
