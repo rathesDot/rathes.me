1e3f26709528b44177505a133ec48fca749c9e4c
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-17 06:44:55 +0200

    💄 Add base styles to icon

--- src/components/fitness/icons/ChevronLeft.js
+++ src/components/fitness/icons/ChevronLeft.js
@@ -1,18 +1,21 @@
 import React from "react"
+import { BaseIcon } from "./BaseIcon"
 
 const ChevronLeft = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="10px"
-    height="16px"
-    fill="none"
-    viewBox="0 0 10 16"
-  >
-    <path
-      fill="#fff"
-      d="M9.283 13.3878a1.6326 1.6326 0 01-2.2857 2.2857L.4667 9.1429a1.6327 1.6327 0 010-2.2858L6.9973.3265A1.6326 1.6326 0 019.283 2.6123L3.928 8l5.3877 5.3878H9.283z"
-    />
-  </svg>
+  <BaseIcon>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10px"
+      height="16px"
+      fill="none"
+      viewBox="0 0 10 16"
+    >
+      <path
+        fill="#fff"
+        d="M9.283 13.3878a1.6326 1.6326 0 01-2.2857 2.2857L.4667 9.1429a1.6327 1.6327 0 010-2.2858L6.9973.3265A1.6326 1.6326 0 019.283 2.6123L3.928 8l5.3877 5.3878H9.283z"
+      />
+    </svg>
+  </BaseIcon>
 )
 
 export { ChevronLeft }
