ca2fecf72edd50e16cb2cb1cf22d59ce449a29d6
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-24 14:25:46 +0200

    🚨 Lint file

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -2,43 +2,44 @@ import React from "react"
 import styled from "styled-components"
 
 const Element = styled.div`
-  ${props =>
+  ${(props) =>
     props.fontFamily
       ? `font-family: ${props.theme.fontFamily[props.fontFamily]};`
       : undefined};
-  ${props =>
+  ${(props) =>
     props.fontSize
       ? `font-size: ${props.theme.fontSize[props.fontSize]};`
       : undefined}
-  ${props =>
+  ${(props) =>
     props.fontWeight ? `font-weight: ${props.fontWeight};` : undefined}
-  ${props =>
+  ${(props) =>
     props.lineHeight ? `line-Height: ${props.lineHeight};` : undefined}
-  ${props =>
+  ${(props) =>
     props.textColor
       ? `color: ${props.theme.colors[props.textColor][props.textTint]};`
       : undefined}
-  ${props =>
+  ${(props) =>
     props.backgroundColor
       ? `background-color: ${
           props.theme.colors[props.backgroundColor][props.backgroundTint]
         };`
       : undefined}
-  display: ${props =>
+  display: ${(props) =>
     props.displayElement ? `${props.displayElement};` : undefined};
-  margin-bottom: ${props => props.theme.spacing[props.marginBottom]};
-  margin-top: ${props => props.theme.spacing[props.marginTop]};
-  ${props => (props.maxWidth ? `max-width: ${props.maxWidth}` : undefined)};
-  ${props => (props.minHeight ? `min-height: ${props.minHeight}` : undefined)};
-  padding: ${props => props.theme.spacing[props.padding]};
-  padding-left: ${props => props.theme.spacing[props.paddingLeft]};
-  ${props =>
+  margin-bottom: ${(props) => props.theme.spacing[props.marginBottom]};
+  margin-top: ${(props) => props.theme.spacing[props.marginTop]};
+  ${(props) => (props.maxWidth ? `max-width: ${props.maxWidth}` : undefined)};
+  ${(props) =>
+    props.minHeight ? `min-height: ${props.minHeight}` : undefined};
+  padding: ${(props) => props.theme.spacing[props.padding]};
+  padding-left: ${(props) => props.theme.spacing[props.paddingLeft]};
+  ${(props) =>
     props.flexDirection ? `flex-direction: ${props.flexDirection}` : undefined};
-  ${props =>
+  ${(props) =>
     props.justifyContent
       ? `justify-content: ${props.justifyContent}`
       : undefined};
-  ${props =>
+  ${(props) =>
     props.alignItems ? `align-items: ${props.alignItems}` : undefined};
 `
 
