37e12670b459e6823af285adc67334a5521cc0f9
Rathes Sachchithananthan <rathes@aheenam.com>
2020-04-24 14:38:56 +0200

    ♻️ Base card of Box component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 
-const Card = styled.div`
+import { Box } from "../Box"
+
+const Card = styled(Box)`
   background-color: ${(props) => props.theme.colors.black.light};
   border: solid 1px ${(props) => props.theme.colors.black.lighter};
   border-radius: 4px;
