1e2ca9f29af085050007b374c6be8b544c928c28
Rathes Sachchithananthan <rathes@aheenam.com>
2020-10-03 00:49:47 +0100

    ✨ Add color variations for text

--- src/library/components/typography/Text.tsx
+++ src/library/components/typography/Text.tsx
@@ -3,6 +3,32 @@ import { styled } from "../../../../stitches.config"
 export const Text = styled("span", {
   color: "$gray600",
   variants: {
+    color: {
+      black: {
+        color: "$black",
+      },
+      white: {
+        color: "$white",
+      },
+      gray400: {
+        color: "$gray400",
+      },
+      gray500: {
+        color: "$gray500",
+      },
+      gray600: {
+        color: "$gray600",
+      },
+      gray700: {
+        color: "$gray700",
+      },
+      gray800: {
+        color: "$gray800",
+      },
+      gray900: {
+        color: "$gray900",
+      },
+    },
     size: {
       body: {
         fontSize: "$base",
