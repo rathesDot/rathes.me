9db5d30b5909f39d09d6529c272fa643663debf6
Rathes Sachchithananthan <rathes@aheenam.com>
2020-11-16 20:51:54 +0000

    🎨 Simplify component

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,11 +1,4 @@
-import React from "react"
 import styled from "styled-components"
-
-const StyledLink = styled.a<LinkProps>`
-  color: ${(props) =>
-    props.color ? props.theme.colors[props.color][props.tint] : undefined};
-  text-decoration: ${(props) => (props.underlined ? "underline" : "none")};
-`
 
 export type LinkProps = {
   color?: "white" | "black" | "silver"
@@ -21,17 +14,8 @@ export type LinkProps = {
   underlined?: boolean
 }
 
-export const Link: React.FC<LinkProps> = ({
-  children,
-  color = undefined,
-  tint = "default",
-  isCurrent = false,
-  underlined = false,
-  ...props
-}) => {
-  return (
-    <StyledLink color={color} tint={tint} underlined={underlined} {...props}>
-      {children}
-    </StyledLink>
-  )
-}
+export const Link = styled.a<LinkProps>`
+  color: ${(props) =>
+    props.color ? props.theme.colors[props.color][props.tint] : undefined};
+  text-decoration: ${(props) => (props.underlined ? "underline" : "none")};
+`
