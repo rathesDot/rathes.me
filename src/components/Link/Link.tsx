0e6c39d1274641e0102901e73c7d4036d99890c6
Rathes Sachchithananthan <rathes@aheenam.com>
2020-11-16 22:33:31 +0000

    🐛 Add missing default tint

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -16,6 +16,8 @@ export type LinkProps = {
 
 export const Link = styled.a<LinkProps>`
   color: ${(props) =>
-    props.color ? props.theme.colors[props.color][props.tint] : undefined};
+    props.color
+      ? props.theme.colors[props.color][props.tint || "default"]
+      : undefined};
   text-decoration: ${(props) => (props.underlined ? "underline" : "none")};
 `
