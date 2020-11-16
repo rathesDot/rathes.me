64587967b14a9342e9480cc045652f3b0c21ae75
Rathes Sachchithananthan <rathes@aheenam.com>
2020-11-16 23:11:34 +0000

    🎨 Simplify component

--- src/components/typography/TextBody.js
+++ src/components/typography/TextBody.js
@@ -1,25 +1,8 @@
-import React from "react"
 import styled from "styled-components"
 
-const Element = styled.span`
+export const TextBody = styled.span`
   font-size: ${(props) => props.theme.fontSize.base};
   color: ${(props) =>
     props.color ? props.theme.colors[props.color][props.tint] : undefined};
   text-decoration: ${(props) => (props.underlined ? "underline" : "none")};
 `
-
-export const TextBody = ({
-  children,
-  color,
-  tint = "default",
-  underlined = false,
-  ...props
-}) => {
-  return (
-    <Element color={color} tint={tint} underlined={underlined} {...props}>
-      {children}
-    </Element>
-  )
-}
-
-export default TextBody
