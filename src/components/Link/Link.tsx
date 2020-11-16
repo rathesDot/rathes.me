3e160fb75dd60ba77529ef6c3847eaaabb83b93a
Rathes Sachchithananthan <rathes@aheenam.com>
2020-11-16 20:50:31 +0000

    🏷 Add LinkProps

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,13 +1,27 @@
 import React from "react"
 import styled from "styled-components"
 
-const StyledLink = styled.a`
+const StyledLink = styled.a<LinkProps>`
   color: ${(props) =>
     props.color ? props.theme.colors[props.color][props.tint] : undefined};
   text-decoration: ${(props) => (props.underlined ? "underline" : "none")};
 `
 
-export const Link = ({
+export type LinkProps = {
+  color?: "white" | "black" | "silver"
+  tint?:
+    | "default"
+    | "light"
+    | "lighter"
+    | "lightest"
+    | "dark"
+    | "darker"
+    | "darkest"
+  isCurrent?: boolean
+  underlined?: boolean
+}
+
+export const Link: React.FC<LinkProps> = ({
   children,
   color = undefined,
   tint = "default",
