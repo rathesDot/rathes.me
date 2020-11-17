0e9b8c6a44e17722ba20ed9c37392825d7c36de2
Rathes Sachchithananthan <rathes@aheenam.com>
2020-11-17 11:09:36 +0000

    🚨 Group imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 
-import { PageLayout } from "../layouts"
 import { Meta } from "../components"
-
 import { OverviewInfo } from "../patterns"
+import { PageLayout } from "../layouts"
 
 const Container = styled.section`
   display: flex;
