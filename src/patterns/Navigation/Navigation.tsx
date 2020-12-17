87d155bc11860d4c57f34d07d3edd889e3a5e425
Rathes Sachchithananthan <rathes@aheenam.com>
2020-12-17 17:50:55 +0000

    ♻️ Replace Separator with styled component

--- src/patterns/Navigation/Navigation.tsx
+++ src/patterns/Navigation/Navigation.tsx
@@ -1,27 +1,37 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
-import styled from "styled-components"
+import sc from "styled-components"
+
+import { styled } from "../../../stitches.config"
 
 import { Logo } from "../../components"
 
-const Separator = styled.span`
-  display: inline-block;
-  margin: 0 8px;
-  color: ${(props) => props.theme.colors.silver.default};
-  font-size: ${(props) => props.theme.fontSize.lg};
-  font-family: ${(props) => props.theme.fontFamily.default};
+const Separator = styled("span", {
+  color: "$gray600",
+  display: "inline-block",
+  fontFamily: "$default",
+  fontSize: "$base",
+  margin: "0 $2",
 
-  @media (max-width: 374px) {
-    margin: 0 4px;
-    font-size: ${(props) => props.theme.fontSize.base};
-  }
+  variants: {
+    size: {
+      default: {
+        margin: "0 $1",
+        fontSize: "$base",
+      },
+      large: {
+        margin: "0 $2",
+        fontSize: "$lg",
+      },
+    },
+  },
 
-  &:before {
-    content: "/";
-  }
-`
+  "::before": {
+    content: "'/'",
+  },
+})
 
-const MenuItem = styled.span`
+const MenuItem = sc.span`
   font-size: ${(props) => props.theme.fontSize.lg};
   font-family: ${(props) => props.theme.fontFamily.default};
   font-weight: 600;
@@ -35,7 +45,7 @@ const MenuItem = styled.span`
   }
 `
 
-const Container = styled.nav`
+const Container = sc.nav`
   align-items: center;
   color: ${(props) => props.theme.colors.silver.default};
   display: flex;
@@ -57,19 +67,19 @@ const Navigation = () => {
           <Logo />
         </MenuItem>
       </Link>
-      <Separator />
+      <Separator size={{ initial: "default", xs: "large" }} />
       <Link activeStyle={{ color: "#FFF" }} to="/about">
         <MenuItem>About</MenuItem>
       </Link>
-      <Separator />
+      <Separator size={{ initial: "default", xs: "large" }} />
       <a href={data.resume.publicURL} aria-label="Resume">
         <MenuItem>Resume</MenuItem>
       </a>
-      <Separator />
+      <Separator size={{ initial: "default", xs: "large" }} />
       <Link activeStyle={{ color: "#FFF" }} to="/work">
         <MenuItem>Work</MenuItem>
       </Link>
-      <Separator />
+      <Separator size={{ initial: "default", xs: "large" }} />
       <Link activeStyle={{ color: "#FFF" }} to="/writings">
         <MenuItem>Writings</MenuItem>
       </Link>
