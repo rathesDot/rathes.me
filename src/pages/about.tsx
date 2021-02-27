6c99ffe1bf75504a01c02b7184c96444b0541c19
Rathes Sachchithananthan <rathes@aheenam.com>
2021-02-27 14:37:09 +0000

    ✨ Move A promised land to read

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -91,12 +91,6 @@ const AboutMe: React.FC<PageProps> = () => (
         <ReadingList>
           <List title="Currently reading">
             <ListItem
-              subtitle="by Barack Obama"
-              link="https://uk.bookshop.org/a/6216/9780241491515"
-            >
-              A Promised Land
-            </ListItem>
-            <ListItem
               subtitle="by George R. R. Martin"
               link="https://amzn.to/2Rngm82"
             >
@@ -108,6 +102,12 @@ const AboutMe: React.FC<PageProps> = () => (
           </List>
           <Separator />
           <List title="Reading List 2021">
+            <ListItem
+              subtitle="by Barack Obama"
+              link="https://uk.bookshop.org/a/6216/9780241491515"
+            >
+              A Promised Land
+            </ListItem>
             <ListItem
               subtitle="by Nir Eyal"
               link="https://uk.bookshop.org/a/6216/9780241184837"
