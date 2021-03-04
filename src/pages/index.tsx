26c47530b9743f5742f4b589af3666fdf8656ffb
Rathes Sachchithananthan <rathes@aheenam.com>
2021-03-04 19:44:56 +0000

    💄 Align items in columns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,15 @@ const Introduction = styled("h1", {
   },
 })
 
+const Lists = styled("div", {
+  marginTop: "$8",
+  display: "flex",
+  flexWrap: "wrap",
+})
+
 const ListContainer = styled(List, {
-  margin: "$8 0",
+  margin: "$8 $4",
+  width: 180,
 })
 
 const IndexPage: React.FC<PageProps> = () => (
@@ -43,47 +50,49 @@ const IndexPage: React.FC<PageProps> = () => (
         Hi there, I’m Rathes Sachchithananthan and I’m a web developer currently
         living in London
       </Introduction>
-      <ListContainer title="About Me">
-        <ListItem subtitle="Working at" link="https://www.hussle.com">
-          Hussle
-        </ListItem>
-        <ListItem
-          subtitle="Living In"
-          link="https://goo.gl/maps/E9c5uw5SLjSbLZ9G9"
-        >
-          London
-        </ListItem>
-        <ListItem
-          subtitle="Working at"
-          link="https://twitter.com/rswebdesigner"
-        >
-          @rswebdesigner
-        </ListItem>
-      </ListContainer>
-      <ListContainer title="Current Projects">
-        <ListItem
-          subtitle="A simple and beautiful way to plan, schedule and track your workouts"
-          link="https://getmaxout.app/"
-        >
-          Maxout
-        </ListItem>
-        <ListItem
-          subtitle="Book and App to make learning the Tamil language easier"
-          link="https://learn-tamil.com/"
-        >
-          Learn Tamil Online
-        </ListItem>
-      </ListContainer>
-      <ListContainer title="My Writing">
-        <ListItem link="https://getmaxout.app/blog/introducing-maxout/">
-          Building Maxout
-        </ListItem>
-        <ListItem link="/blog/en/review-2020/">Looking back at 2020</ListItem>
-        <ListItem link="https://getmaxout.app/blog/introducing-maxout/">
-          Introducing Maxout
-        </ListItem>
-        <ListItem link="/blog/en/how-to-slack/">How-To Slack</ListItem>
-      </ListContainer>
+      <Lists>
+        <ListContainer title="About Me">
+          <ListItem subtitle="Working at" link="https://www.hussle.com">
+            Hussle
+          </ListItem>
+          <ListItem
+            subtitle="Living In"
+            link="https://goo.gl/maps/E9c5uw5SLjSbLZ9G9"
+          >
+            London
+          </ListItem>
+          <ListItem
+            subtitle="Working at"
+            link="https://twitter.com/rswebdesigner"
+          >
+            @rswebdesigner
+          </ListItem>
+        </ListContainer>
+        <ListContainer title="Current Projects">
+          <ListItem
+            subtitle="A simple and beautiful way to plan, schedule and track your workouts"
+            link="https://getmaxout.app/"
+          >
+            Maxout
+          </ListItem>
+          <ListItem
+            subtitle="Book and App to make learning the Tamil language easier"
+            link="https://learn-tamil.com/"
+          >
+            Learn Tamil Online
+          </ListItem>
+        </ListContainer>
+        <ListContainer title="My Writing">
+          <ListItem link="https://getmaxout.app/blog/introducing-maxout/">
+            Building Maxout
+          </ListItem>
+          <ListItem link="/blog/en/review-2020/">Looking back at 2020</ListItem>
+          <ListItem link="https://getmaxout.app/blog/introducing-maxout/">
+            Introducing Maxout
+          </ListItem>
+          <ListItem link="/blog/en/how-to-slack/">How-To Slack</ListItem>
+        </ListContainer>
+      </Lists>
     </Container>
   </PageLayout>
 )
