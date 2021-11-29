c56429778fb2bfefea84b2d2cc480db0b2f0b967
Rathes Sachchithananthan <rathes@aheenam.com>
2021-11-29 14:21:18 +0000

    🔥 Remove now unused creation of node field

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,1 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const {
-    internal: { type },
-  } = node
-  const { createNodeField } = actions
-
-  if (["MarkdownRemark", "Mdx"].includes(type)) {
-    const filePath = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: filePath.replace("/articles/", "/blog/"),
-    })
-  }
-}
+//
