2b704474dd9381ee6cfc31a0788f18d1815a6b91
Rathes Sachchithananthan <rathes@aheenam.com>
2021-12-01 09:57:16 +0000

    ♻️ Replace gray800 with slate6

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,7 +1,7 @@
 import { styled } from "../../../stitches.config"
 
 export const Separator = styled("hr", {
-  borderColor: "$gray800",
+  borderColor: "$slate6",
   borderWidth: 2,
   margin: "$8 0",
   width: 24,
