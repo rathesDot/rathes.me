fcf28987bf52f35279f72c837974ba30bfc72d4c
Rathes Sachchithananthan <rathes@aheenam.com>
2021-12-07 16:32:25 +0000

    🐛 Add missing MDX provider

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,19 @@
 import React from "react"
+import { MDXProvider } from "@mdx-js/react"
 import { renderToString } from "react-dom/server"
 
 import { getCssText } from "./stitches.config"
+
+import {
+  Heading,
+  Link,
+  OrderedList,
+  Paragraph,
+  Quote,
+  Separator,
+} from "./src/components"
+
+import "prism-themes/themes/prism-atom-dark.css"
 
 export const replaceRenderer = ({
   bodyComponent,
@@ -22,3 +34,28 @@ export const replaceRenderer = ({
     replaceBodyHTMLString(bodyHTML),
   ])
 }
+
+const H1 = ({ children }) => <Heading level="heading1">{children}</Heading>
+const H2 = ({ children }) => <Heading level="heading2">{children}</Heading>
+const H3 = ({ children }) => <Heading level="heading3">{children}</Heading>
+const H4 = ({ children }) => <Heading level="heading4">{children}</Heading>
+
+const components = {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  hr: Separator,
+  p: Paragraph,
+  ol: OrderedList,
+  blockquote: Quote,
+  a: ({ children, ...props }) => (
+    <Link underlined color="white" {...props}>
+      {children}
+    </Link>
+  ),
+}
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+)
