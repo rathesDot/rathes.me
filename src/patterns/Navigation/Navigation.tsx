eb59e68b4bb713203a2ffe04a5f7c0a4c1e0cbda
Rathes Sachchithananthan <rathes@aheenam.com>
2021-12-11 18:32:14 +0000

    🐛 Add key on loop

--- src/patterns/Navigation/Navigation.tsx
+++ src/patterns/Navigation/Navigation.tsx
@@ -92,8 +92,9 @@ const Hamburger: React.FC<{ isOpen: boolean; onToggle: () => void }> = ({
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        {iconPaths.map((p) => (
+        {iconPaths.map((p, i) => (
           <path
+            key={i}
             d={p}
             stroke="currentColor"
             strokeWidth="1.5"
