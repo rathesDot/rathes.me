30f62ae34d0b4f3affa781b289ea44b4599eb595
Rathes Sachchithananthan <rathes@aheenam.com>
2021-12-16 08:31:40 +0000

    ✨ Introduce CodeBlock component

---
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,8 @@
+import { styled } from "../../../stitches.config"
+
+export const CodeBlock = styled("pre", {
+  background: "$slate4 !important",
+  borderRadius: "10px",
+  margin: "$4 -$8 !important",
+  overflow: "hidden",
+})
