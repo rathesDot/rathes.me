23b225baa03d53878981be4bfc84b19446c9a9d6
Rathes Sachchithananthan <rathes@aheenam.com>
2021-12-16 18:21:02 +0000

    💄 Improve colors and spacing

--- src/components/CodeBlock/InlineCode.tsx
+++ src/components/CodeBlock/InlineCode.tsx
@@ -1,11 +1,11 @@
 import { styled } from "../../../stitches.config"
 
 export const InlineCode = styled("code", {
-  background: "$slate3",
+  background: "$slate6",
   color: "$slate12",
   fontFamily: "$mono",
   fontWeight: 400,
-  padding: "0.1em",
-  borderRadius: "0.3em",
+  padding: "0.1em $2",
+  borderRadius: "4px",
   whiteSpace: "normal",
 })
