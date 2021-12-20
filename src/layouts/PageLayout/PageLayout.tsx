d52841b4e7bbfc7734573d53d4a2162eea53ead7
Rathes Sachchithananthan <rathes@aheenam.com>
2021-12-20 08:08:31 +0000

    ✨ Introduce helper to determine color mode

--- src/layouts/PageLayout/PageLayout.tsx
+++ src/layouts/PageLayout/PageLayout.tsx
@@ -13,6 +13,24 @@ export const ThemeContext = createContext<ThemeContextType>(
   {} as ThemeContextType
 )
 
+const getInitialColorMode = () => {
+  const persistedColorPreference = window.localStorage.getItem("color-mode")
+  const hasPersistedPreference = typeof persistedColorPreference === "string"
+
+  if (hasPersistedPreference) {
+    return persistedColorPreference
+  }
+
+  const mql = window.matchMedia("(prefers-color-scheme: dark)")
+  const hasMediaQueryPreference = typeof mql.matches === "boolean"
+
+  if (hasMediaQueryPreference) {
+    return mql.matches ? "dark" : "light"
+  }
+
+  return "dark"
+}
+
 const MainContainer = styled("main", {
   backgroundColor: "$slate2",
   color: "$slate11",
