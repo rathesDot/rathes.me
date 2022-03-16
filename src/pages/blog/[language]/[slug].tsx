82261e200eb0fcd23d4d6977853adebc7ce9a029
Rathes Sachchithananthan <rathes@aheenam.com>
2022-03-16 21:35:11 +0000

    💄 Render blog post base

--- src/pages/blog/[language]/[slug].tsx
+++ src/pages/blog/[language]/[slug].tsx
@@ -4,7 +4,8 @@ import {
   NextPage,
   InferGetStaticPropsType,
 } from "next"
-import { PageLayout } from "../../../layouts"
+import Image from "next/image"
+import Link from "next/link"
 import { serialize } from "next-mdx-remote/serialize"
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
 
@@ -12,11 +13,90 @@ import path from "path"
 import fs from "fs"
 import matter from "gray-matter"
 
+import { styled } from "../../../../stitches.config"
+
+import { PageLayout } from "../../../layouts"
+import { Heading } from "../../../components"
+
+const Container = styled("div", {
+  maxWidth: 640,
+})
+
+const Title = styled(Heading, {
+  color: "$slate12",
+  fontFamily: "$display",
+  fontWeight: 600,
+  lineHeight: "$400",
+  overflowWrap: "break-word",
+  maxWidth: 640,
+  marginBottom: "$2",
+  marginTop: "$12",
+
+  variants: {
+    size: {
+      small: {
+        fontSize: "$lg",
+      },
+      default: {
+        fontSize: "$xl",
+      },
+      large: {
+        fontSize: "$2xl",
+      },
+    },
+  },
+})
+
+const FeatureImage = styled("div", {
+  borderRadius: "10px",
+  margin: "$4 -$8",
+  overflow: "hidden",
+  position: "relative",
+  height: 320,
+
+  img: {
+    objectFit: "cover",
+  },
+})
+
+const Footer = styled("footer", {
+  marginBottom: "$32",
+  marginTop: "$32",
+})
+
+const BackLink = styled("span", {
+  color: "$slate12",
+  textDecoration: "underline",
+})
+
 const Blogpost: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   source,
+  frontmatter,
 }) => (
   <PageLayout>
-    <MDXRemote {...source} />
+    {/** @todo Add meta data of blog */}
+    <Container>
+      {/** @todo Improve image sizes */}
+      {frontmatter.image && (
+        <FeatureImage>
+          <Image src={frontmatter.image} layout="fill" />
+        </FeatureImage>
+      )}
+      <Title
+        size={{ "@initial": "small", "@xs": "default", "@sm": "large" }}
+        level="heading1"
+      >
+        {frontmatter.title}
+      </Title>
+      <MDXRemote {...source} />
+      <Footer>
+        <Link href="/writings">
+          <a>
+            <BackLink>back to articles</BackLink>
+          </a>
+        </Link>
+      </Footer>
+    </Container>
   </PageLayout>
 )
 
@@ -45,6 +125,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps<{
   source: MDXRemoteSerializeResult
+  frontmatter: { [key: string]: any }
 }> = async ({ params }) => {
   const language = params.language
   const slug = params.slug
@@ -54,10 +135,11 @@ export const getStaticProps: GetStaticProps<{
     "utf-8"
   )
 
-  const { content } = matter(source)
+  const { content, data } = matter(source)
 
   return {
     props: {
+      frontmatter: JSON.parse(JSON.stringify(data)),
       source: await serialize(content),
     },
   }
