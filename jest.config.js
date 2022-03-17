087b661b7e7a86af582a9baef7f18c04a97c20fc
Rathes Sachchithananthan <rathes@aheenam.com>
2022-03-17 16:31:45 +0000

    🔥 Remove last occurence of gatsby --- Excluding the README which will be updated at some later point

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
     "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `.cache`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
