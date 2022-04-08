bac5263cce86af31cbf96586597a7b4de60f0eed
Rathes Sachchithananthan <rathes@aheenam.com>
2022-04-08 21:38:46 +0100

    🐛 Make wrapper a span --- This way, we can have the image wrapper within paragraphs

--- src/components/Image/ImageWrapper.tsx
+++ src/components/Image/ImageWrapper.tsx
@@ -1,7 +1,8 @@
 import { styled } from "../../../stitches.config"
 
-export const ImageWrapper = styled("div", {
+export const ImageWrapper = styled("span", {
   borderRadius: "10px",
+  display: "block",
   margin: "$4 -$8",
   overflow: "hidden",
   position: "relative",
