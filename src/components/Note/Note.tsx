583aa3e024d4c68de05e740f65cab14b2d5c83bd
Rathes Sachchithananthan <rathes@aheenam.com>
2022-11-02 15:37:27 +0000

    🐛 Fix p was nested in p

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,9 +1,10 @@
 import { styled } from "../../../stitches.config"
 
-export const Note = styled("p", {
+export const Note = styled("span", {
   backgroundColor: "$slate3",
   borderRadius: "10px",
   color: "$slate11",
+  display: "block",
   lineHeight: "$500",
   margin: "$8 -$8",
   padding: "$8",
