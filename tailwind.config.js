0da02710cce79d1de9c2a4b618ef1e7783ea5d63
Rathes Sachchithananthan <rathes@aheenam.com>
2022-11-11 10:16:30 +0000

    🔧 Add Noto Sans as fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-notoSans)", ...fontFamily.sans],
+        sansDisplay: ["var(--font-notoDisplay)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
