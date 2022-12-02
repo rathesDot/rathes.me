406dcd1bf62b9cc9f6020f03d0c3e173ae4d825b
Rathes Sachchithananthan <rathes@aheenam.com>
2022-12-02 09:20:49 +0000

    🐛 Avoid nesting div in p

--- src/components/Image/ImageWrapper.tsx
+++ src/components/Image/ImageWrapper.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react"
 
 export const ImageWrapper: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="relative my-4 -mx-8 overflow-hidden rounded-lg [&_img]:object-cover [&_img]:object-center">
+  <span className="relative my-4 -mx-8 overflow-hidden rounded-lg [&_img]:object-cover [&_img]:object-center">
     {children}
-  </div>
+  </span>
 )
