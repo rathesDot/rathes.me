1f17571563f0fc4eb26afecea8bc49bf5e4639cd
Rathes Sachchithananthan <rathes@aheenam.com>
2022-12-02 09:24:13 +0000

    🐛 Ensure long code doesn't break view

--- src/components/CodeBlock/InlineCode.tsx
+++ src/components/CodeBlock/InlineCode.tsx
@@ -2,7 +2,7 @@ import { HTMLProps } from "react"
 
 export const InlineCode: React.FC<HTMLProps<HTMLElement>> = ({ ...props }) => (
   <code
-    className="whitespace-normal rounded bg-neutral-200 py-[0.1em] px-2 font-mono text-neutral-900 dark:bg-neutral-800 dark:text-neutral-200"
+    className="whitespace-normal break-all rounded bg-neutral-200 py-[0.1em] px-2 font-mono text-neutral-900 dark:bg-neutral-800 dark:text-neutral-200"
     {...props}
   />
 )
