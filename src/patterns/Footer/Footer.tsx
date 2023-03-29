294e7956bd86e1fbc469a6940afe36b5531dccdc
Rathes Sachchithananthan <rathes@aheenam.com>
2023-03-29 11:40:09 +0100

    ✨ Update footer year

--- src/patterns/Footer/Footer.tsx
+++ src/patterns/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => (
   <footer className="text-xs text-neutral-400 dark:text-neutral-600">
-    &copy; 2020 — Rathes Sachchithananthan
+    &copy; 2023 — Rathes Sachchithananthan
   </footer>
 )
 
