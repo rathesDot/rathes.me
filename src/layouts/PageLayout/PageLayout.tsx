e432d210cce5fe364c89afd1069f9405f65b5253
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-21 20:09:31 +0000

    💄 Reset existing styles

--- src/layouts/PageLayout/PageLayout.tsx
+++ src/layouts/PageLayout/PageLayout.tsx
@@ -6,9 +6,9 @@ const PageLayout: React.FC<React.PropsWithChildren<unknown>> = ({
   children,
 }) => {
   return (
-    <main className="bg-neutral-50 px-8 font-sans text-neutral-900 dark:bg-neutral-900 dark:text-neutral-50">
+    <main className="font-sans">
       <Navigation />
-      <div className="mx-auto flex min-h-screen max-w-xl flex-col justify-between pt-32">
+      <div>
         {children}
         <Footer />
       </div>
