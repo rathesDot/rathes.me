0e724095837c094e342a9a17fd638323643ac067
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 13:21:53 +0000

    💄 Improve sizes and spaces

--- src/patterns/SayHi/SayHi.tsx
+++ src/patterns/SayHi/SayHi.tsx
@@ -3,11 +3,11 @@ import React from "react"
 import { Link } from "../../components"
 
 export const SayHi: React.FC<React.PropsWithChildren<unknown>> = () => (
-  <div className="max-w-lg font-sansDisplay text-4xl font-semibold text-neutral-900 dark:text-neutral-50">
-    Work inquiry, question or something else?{" "}
+  <div className="max-w-lg font-sans text-4xl font-bold leading-none text-neutral-50 antialiased">
+    Work inquiry, question or something else? <br />
     <Link
-      aria-label="Send an E-Mail to hello@rathes.me"
-      href="mailto:hello@rathes.me"
+      aria-label="Send an E-Mail to hello+website@rathes.me"
+      href="mailto:hello+website@rathes.me"
       underlined
     >
       Say hi!
