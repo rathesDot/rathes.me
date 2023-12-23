9b6d596bc59740f23a288e8dea9c9282f7145f88
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 14:08:33 +0000

    💄 Adjust styles

--- src/components/Form/TextField.tsx
+++ src/components/Form/TextField.tsx
@@ -5,6 +5,6 @@ export const TextField: React.FC<InputHTMLAttributes<HTMLInputElement>> = (
 ) => (
   <input
     {...props}
-    className="box-border inline-flex h-9 w-full max-w-md items-center justify-center rounded-lg bg-neutral-200 px-2 text-base text-neutral-900 outline-none ring-1 ring-neutral-200 focus:ring-neutral-400 dark:bg-neutral-800 dark:text-neutral-50 dark:ring-neutral-800 dark:focus:ring-neutral-700"
+    className="box-border inline-flex h-9 w-full max-w-md items-center justify-center rounded-lg bg-neutral-800 px-2 text-base text-neutral-50/70 outline-none ring-1 ring-neutral-800 placeholder:text-neutral-500 focus:ring-neutral-700/50"
   />
 )
