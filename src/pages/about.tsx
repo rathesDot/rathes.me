9d77bb350ee5607f11ff46583f7b05bdb0187641
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 13:18:44 +0000

    ✨ Hide long text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import RouterLink from "next/link"
 import { NextPage } from "next"
 
@@ -16,39 +16,63 @@ import {
 
 import { SayHi } from "../patterns"
 
-import books from "../../data/books.json"
+import { cva } from "class-variance-authority"
 
-const AboutMe: React.FC<React.PropsWithChildren<NextPage>> = () => (
-  <PageLayout>
-    <Meta title="About me" />
-    <div className="max-w-xl p-8">
-      <Heading1 size={2}>
-        I'm a front-end engineer with over 10 years of web experience.
-      </Heading1>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit
-        nunc massa, condimentum accumsan lectus luctus vitae. Vestibulum commodo
-        porta neque eget semper. Phasellus non dictum metus. Pellentesque
-        pretium est quis aliquet tempus.
-      </Paragraph>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit
-        nunc massa, condimentum accumsan lectus luctus vitae. Vestibulum commodo
-        porta neque eget semper. Phasellus non dictum metus. Pellentesque
-        pretium est quis aliquet tempus.
-      </Paragraph>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit
-        nunc massa, condimentum accumsan lectus luctus vitae. Vestibulum commodo
-        porta neque eget semper. Phasellus non dictum metus. Pellentesque
-        pretium est quis aliquet tempus.
-      </Paragraph>
-
-      <div className="my-32">
-        <SayHi />
-      </div>
-    </div>
-  </PageLayout>
+const readMore = cva(
+  "relative delay-100 after:h-8 after:bg-gradient-to-t after:from-neutral-950 after:absolute after:bottom-0 after:inset-x-0 transition-all overflow-hidden",
+  {
+    variants: {
+      open: {
+        false: "max-h-56 after:opacity-100",
+        true: "max-h-[540px] after:opacity-0",
+      },
+    },
+  }
 )
+
+const AboutMe: React.FC<React.PropsWithChildren<NextPage>> = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <PageLayout>
+      <Meta title="About me" />
+      <div className="max-w-xl p-8">
+        <Heading1 size={2}>
+          I'm a front-end engineer with over 10 years of web experience.
+        </Heading1>
+        <div className={readMore({ open })}>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            suscipit nunc massa, condimentum accumsan lectus luctus vitae.
+            Vestibulum commodo porta neque eget semper. Phasellus non dictum
+            metus. Pellentesque pretium est quis aliquet tempus.
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            suscipit nunc massa, condimentum accumsan lectus luctus vitae.
+            Vestibulum commodo porta neque eget semper. Phasellus non dictum
+            metus. Pellentesque pretium est quis aliquet tempus.
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            suscipit nunc massa, condimentum accumsan lectus luctus vitae.
+            Vestibulum commodo porta neque eget semper. Phasellus non dictum
+            metus. Pellentesque pretium est quis aliquet tempus.
+          </Paragraph>
+        </div>
+        <button
+          type="button"
+          onClick={() => setOpen(!open)}
+          className="mt-4 inline-block text-white underline underline-offset-2"
+        >
+          {open ? "Read less" : "Continue Reading"}
+        </button>
+
+        <div className="my-32">
+          <SayHi />
+        </div>
+      </div>
+    </PageLayout>
+  )
+}
 
 export default AboutMe
