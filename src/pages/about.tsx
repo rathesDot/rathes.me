381725cce65d4ab04df960abde5cefa384de03e6
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 12:53:17 +0000

    🤡 Use dummy content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -26,117 +26,24 @@ const AboutMe: React.FC<React.PropsWithChildren<NextPage>> = () => (
         I'm a front-end engineer with over 10 years of web experience.
       </Heading1>
       <Paragraph>
-        I'm Rathes Sachchithananthan, a web developer with interests not only in
-        engineering but also in product development & design, user & customer
-        experience, and most importantly in languages and cultures.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit
+        nunc massa, condimentum accumsan lectus luctus vitae. Vestibulum commodo
+        porta neque eget semper. Phasellus non dictum metus. Pellentesque
+        pretium est quis aliquet tempus.
       </Paragraph>
       <Paragraph>
-        I started working as a web designer and web developer at a very young
-        age. I earned my first money as a web designer creating custom MySpace
-        layouts when I was just 14. In 2008, at the age of 16, I founded my own
-        company for web design and development. Since then, I have worked with
-        several clients doing quite a wide range of tasks.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit
+        nunc massa, condimentum accumsan lectus luctus vitae. Vestibulum commodo
+        porta neque eget semper. Phasellus non dictum metus. Pellentesque
+        pretium est quis aliquet tempus.
       </Paragraph>
       <Paragraph>
-        For more than 10 years, I have been working as a web developer doing
-        problem-solving as a back-end engineer using languages such as PHP,
-        Python or Node.js but also as a front-end developer working with
-        HTML/CSS and JavaScript.
-      </Paragraph>
-      <Paragraph>
-        However, my real passion lies beyond software and development. I'm more
-        into solving problems for people, make their lives easier and connecting
-        them with each other. Alongside that, I'm passionate about learning new
-        languages and cultures.
-      </Paragraph>
-      <Paragraph>
-        Nowadays, I work as a product engineer at Intercom in London building
-        web applications.
-      </Paragraph>
-      <Paragraph>
-        Apart from that, I spent most of my time working on smaller side
-        projects at Aheenam. Right now, I'm working on providing a platform for
-        people to learn my mother language Tamil.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit
+        nunc massa, condimentum accumsan lectus luctus vitae. Vestibulum commodo
+        porta neque eget semper. Phasellus non dictum metus. Pellentesque
+        pretium est quis aliquet tempus.
       </Paragraph>
 
-      <div className="mt-32">
-        <Heading2>What I read</Heading2>
-        <Paragraph>
-          Books play an important part in my life. Just sitting on my couch or
-          bed and reading a novel can be really inspiring.
-        </Paragraph>
-        <Paragraph>
-          I do fancy crime novels with interesting detective characters from
-          good old Sherlock Holmes to Harry Hole. But I read basically any kind
-          of book that can broaden one's horizons.
-        </Paragraph>
-        <Paragraph>
-          Actually, I dream of having my own library one day where I can invite
-          everybody to pick up a nice book and read.
-        </Paragraph>
-        <div className="my-12">
-          <List title="Currently reading">
-            {books["Currently reading"].map((book, index) => (
-              <ListItem
-                key={`${book.title.replaceAll(" ", "-")}-${index}`}
-                subtitle={`by ${book.author}`}
-                link={book.url}
-              >
-                {book.title}
-              </ListItem>
-            ))}
-          </List>
-          <Separator />
-          <List title="Reading List 2023">
-            {books["Reading List 2023"].map((book, index) => (
-              <ListItem
-                key={`${book.title.replaceAll(" ", "-")}-${index}`}
-                subtitle={`by ${book.author}`}
-                link={book.url}
-              >
-                {book.title}
-              </ListItem>
-            ))}
-          </List>
-        </div>
-        <Paragraph>
-          I have got an entire{" "}
-          <RouterLink
-            href="/reading-list"
-            className="text-neutral-900 underline underline-offset-2 hover:no-underline dark:text-neutral-50"
-          >
-            Reading List
-          </RouterLink>{" "}
-          with all the books I've read since 2017. Feel free to have a look, you
-          might be able to find the next book for yourself.
-        </Paragraph>
-      </div>
-
-      <div className="mt-32">
-        <Heading2>What I write</Heading2>
-        <Paragraph>
-          From time to time I also write. It can be work related topics that I
-          share in the{" "}
-          <RouterLink
-            href="/writings"
-            className="text-neutral-900 underline underline-offset-2 hover:no-underline dark:text-neutral-50"
-          >
-            writing section
-          </RouterLink>{" "}
-          of this website but I also write to empty my mind and eternalize
-          ideas.
-        </Paragraph>
-        <Paragraph>
-          I have several ideas for some crime novel and I even developed my
-          detective character “Chris Garner”. I want to publish my first short
-          stories soon.
-        </Paragraph>
-        <Paragraph>
-          Before I can do that I have to finish my current book which is called
-          “A guide to basic Tamil Grammar”. I'm writing that book as an entry
-          point for my learning platform.
-        </Paragraph>
-      </div>
       <div className="my-32">
         <SayHi />
       </div>
