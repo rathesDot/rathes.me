806f7f7a671a1a58162586e9d9cc8df31ba16290
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 13:28:58 +0000

    ♻️ Extract subtitle to component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -5,9 +5,13 @@ import { Heading2, Link } from ".."
 
 export type ListItemProps = {
   subtitle?: string
-  link: string
+  link?: string
   subtitlePosition?: "above" | "under"
 }
+
+const Subtitle: React.FC<{ children: string }> = ({ children }) => (
+  <span className="block text-xs text-neutral-500">{children}</span>
+)
 
 export const ListItem: React.FC<React.PropsWithChildren<ListItemProps>> = ({
   children,
@@ -22,21 +26,21 @@ export const ListItem: React.FC<React.PropsWithChildren<ListItemProps>> = ({
       {isExternalLink ? (
         <Link href={link}>
           {subtitle && subtitlePosition == "above" && (
-            <span className="block text-xs text-neutral-500">{subtitle}</span>
+            <Subtitle>{subtitle}</Subtitle>
           )}
           {children}
           {subtitle && subtitlePosition == "under" && (
-            <span className="block text-xs text-neutral-500">{subtitle}</span>
+            <Subtitle>{subtitle}</Subtitle>
           )}
         </Link>
       ) : (
         <RouterLink href={link}>
           {subtitle && subtitlePosition == "above" && (
-            <span className="block text-xs text-neutral-500">{subtitle}</span>
+            <Subtitle>{subtitle}</Subtitle>
           )}
           {children}
           {subtitle && subtitlePosition == "under" && (
-            <span className="block text-xs text-neutral-500">{subtitle}</span>
+            <Subtitle>{subtitle}</Subtitle>
           )}
         </RouterLink>
       )}
