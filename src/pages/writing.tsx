223691f1f1ba135b8e2df6419bf0655ce03813af
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 14:23:45 +0000

    💄 Group intro section

--- src/pages/writing.tsx
+++ src/pages/writing.tsx
@@ -5,7 +5,7 @@ import matter from "gray-matter"
 import path from "path"
 import fs from "fs"
 
-import { Link, List, ListItem, Paragraph, Meta } from "../components"
+import { Link, List, ListItem, Paragraph, Meta, Heading1 } from "../components"
 import { SayHi } from "../patterns"
 import { PageLayout } from "../layouts"
 
@@ -18,34 +18,37 @@ const Writings: React.FC<
 > = ({ blogPosts }) => {
   return (
     <PageLayout>
-      <section className="max-w-xl">
+      <section className="max-w-xl p-8">
         <Meta title="Writings" />
-        <Paragraph>
-          From time to time, I do write. Sometime on my blog, but also on other
-          blogs such as on{" "}
-          <Link
-            href="https://medium.com/@rathes"
-            underlined
-            className="text-neutral-900 dark:text-neutral-50"
-          >
-            Medium
-          </Link>
-          . The topics, I write about are diverse. Obviously, I write about the
-          web, but also about non-web topics like my mother tongue Tamil or even
-          about movies or anime.
-        </Paragraph>
-        <Paragraph>
-          I do write in English, German and Tamil. So if you do not understand
-          the title, it may be that you do not speak the language. If you want
-          me to translate a specific article, just drop a message{" "}
-          <Link
-            href="https://twitter.com/rswebdesigner"
-            underlined
-            className="text-neutral-900 dark:text-neutral-50"
-          >
-            @rswebdesigner
-          </Link>
-        </Paragraph>
+        <section>
+          <Heading1 size={2}>Writing</Heading1>
+          <Paragraph>
+            From time to time, I do write. Sometime on my blog, but also on
+            other blogs such as on{" "}
+            <Link
+              href="https://medium.com/@rathes"
+              underlined
+              className="text-neutral-50"
+            >
+              Medium
+            </Link>
+            . The topics, I write about are diverse. Obviously, I write about
+            the web, but also about non-web topics like my mother tongue Tamil
+            or even about movies or anime.
+          </Paragraph>
+          <Paragraph>
+            I do write in English, German and Tamil. So if you do not understand
+            the title, it may be that you do not speak the language. If you want
+            me to translate a specific article, just drop a message{" "}
+            <Link
+              href="https://twitter.com/rswebdesigner"
+              underlined
+              className="text-neutral-50"
+            >
+              @rswebdesigner
+            </Link>
+          </Paragraph>
+        </section>
 
         <section className="mt-32">
           <div className="mt-12">
