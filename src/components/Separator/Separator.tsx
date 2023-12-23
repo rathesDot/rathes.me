83b569edb75a924f990fe000c55a652817760955
Rathes Sachchithananthan <rathes@aheenam.com>
2023-12-23 14:11:58 +0000

    💄 Update styles

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -2,9 +2,7 @@ import { HTMLAttributes } from "react"
 
 import { cva } from "class-variance-authority"
 
-const styles = cva(
-  "my-8 w-6 border-2 border-neutral-300 dark:border-neutral-700"
-)
+const styles = cva("my-8 w-6 border-2 border-neutral-700/90")
 
 export const Separator: React.FC<HTMLAttributes<HTMLHRElement>> = ({
   className,
