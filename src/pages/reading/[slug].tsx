b2749f736184666bc79491ba256dedb3ff13aad6
Rathes Sachchithananthan <rathes@aheenam.com>
2024-01-07 08:17:16 +0000

    ✨ Fetch correct book

--- src/pages/reading/[slug].tsx
+++ src/pages/reading/[slug].tsx
@@ -1,15 +1,55 @@
 import React from "react"
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next"
+
+import data from "../../../data/books.json"
+
+import {
+  Book,
+  createSlug,
+  findBookBySlug,
+  getAllBooks,
+} from "../../utils/books"
 
 import { PageLayout } from "../../layouts"
+
 import { Meta, Heading1 } from "../../components"
 
-const BookDetailsPage = () => (
+const BookDetailsPage: NextPage<
+  InferGetStaticPropsType<typeof getStaticProps>
+> = ({ title, author }) => (
   <PageLayout>
     <Meta title="Book: " />
     <div className="max-w-xl p-8 md:px-14">
-      <Heading1>Book Details page</Heading1>
+      <Heading1>{title}</Heading1>
+      <p>by {author}</p>
     </div>
   </PageLayout>
 )
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: getAllBooks(data).map((book) => ({
+      params: { slug: createSlug(book) },
+    })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<Book> = async ({ params }) => {
+  const slug = params.slug
+
+  try {
+    const book = findBookBySlug(`${slug}`, getAllBooks(data))
+
+    return { props: { ...book } }
+  } catch (e) {
+    return { notFound: true }
+  }
+}
 
 export default BookDetailsPage
