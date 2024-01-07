1646312d06c3f1e9aeee44e21f9915efe1eec791
Rathes Sachchithananthan <rathes@aheenam.com>
2024-01-07 07:44:47 +0000

    ✅ Ensure it creates slugs

--- src/utils/books.spec.ts
+++ src/utils/books.spec.ts
@@ -1,4 +1,10 @@
+import { createSlug } from "./books"
+
 describe("books", () => {
-  it.todo("creates slug with title and author")
+  it("creates slug with title and author", () => {
+    expect(
+      createSlug({ title: "Book 1", url: "https://abc.com", author: "Author" })
+    ).toBe("book-1-author")
+  })
   it.todo("finds book by title and author")
 })
