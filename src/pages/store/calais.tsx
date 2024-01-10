205f94b19b1775c9133d31ad8b1a9f8b6021abd7
Rathes Sachchithananthan <rathes@aheenam.com>
2024-01-10 17:50:44 +0000

    ✨ Keep content sticky when having additional images

--- src/pages/store/calais.tsx
+++ src/pages/store/calais.tsx
@@ -33,33 +33,35 @@ const IndexPage: React.FC<React.PropsWithChildren<NextPage>> = () => (
           className="hidden md:block"
         />
       </div>
-      <div className=" max-w-md px-8 py-4">
-        <Heading1>Calais — Figma Template</Heading1>
-        <div className="my-2 text-neutral-50/60">£9.99</div>
-        <div className="my-4">
-          <Link
-            href="https://aheenam.lemonsqueezy.com/checkout/buy/597d3764-92f3-4db7-a551-b17ac34a3d77?discount=0"
-            className="block w-full cursor-pointer rounded bg-neutral-800/80 px-4 py-3 text-center font-semibold text-neutral-50 antialiased hover:bg-neutral-700/50"
-          >
-            Buy Now
-          </Link>
+      <div className="max-w-md px-8 py-4">
+        <div className="md:sticky md:top-32">
+          <Heading1>Calais — Figma Template</Heading1>
+          <div className="my-2 text-neutral-50/60">£9.99</div>
+          <div className="my-4">
+            <Link
+              href="https://aheenam.lemonsqueezy.com/checkout/buy/597d3764-92f3-4db7-a551-b17ac34a3d77?discount=0"
+              className="block w-full cursor-pointer rounded bg-neutral-800/80 px-4 py-3 text-center font-semibold text-neutral-50 antialiased hover:bg-neutral-700/50"
+            >
+              Buy Now
+            </Link>
+          </div>
+          <Paragraph>
+            A minimal personal portfolio template best for someone who wants to
+            showcase their projects but also have their resume ready on their
+            website.
+          </Paragraph>
+
+          <Paragraph>
+            This template comes with 7 pages (Home, About, Work, Case Study,
+            Blog, Single Blog Post and Contact) designed for mobile and desktop
+            screen sizes. The key elements have been separated as components so
+            you can modify them as you like.
+          </Paragraph>
+
+          <Paragraph className="italic text-neutral-50/45">
+            You will get a Calais.fig (41.3MB) file.
+          </Paragraph>
         </div>
-        <Paragraph>
-          A minimal personal portfolio template best for someone who wants to
-          showcase their projects but also have their resume ready on their
-          website.
-        </Paragraph>
-
-        <Paragraph>
-          This template comes with 7 pages (Home, About, Work, Case Study, Blog,
-          Single Blog Post and Contact) designed for mobile and desktop screen
-          sizes. The key elements have been separated as components so you can
-          modify them as you like.
-        </Paragraph>
-
-        <Paragraph className="italic text-neutral-50/45">
-          You will get a Calais.fig (41.3MB) file.
-        </Paragraph>
       </div>
     </section>
   </PageLayout>
