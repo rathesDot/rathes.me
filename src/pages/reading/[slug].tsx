f8132f942f70758e2081b93c1d746b2c7c39b474
Rathes Sachchithananthan <rathes@aheenam.com>
2024-01-11 08:26:43 +0000

    💄 Improve spacing and sizes on desktop

--- src/pages/reading/[slug].tsx
+++ src/pages/reading/[slug].tsx
@@ -51,12 +51,12 @@ const BookDetailsPage: NextPage<
   return (
     <PageLayout>
       <Meta title="Book: " />
-      <div className="flex max-w-xl flex-col gap-4 p-8 md:px-14">
+      <div className="flex max-w-xl flex-col gap-4 p-8 md:gap-6 md:px-14">
         <header>
           <Heading1>{title}</Heading1>
           <p className="text-base text-neutral-300/80">by {author}</p>
         </header>
-        <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
+        <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-x-2 md:gap-y-6">
           <div className="col-start-1 col-end-1 row-start-1 row-end-1 md:flex md:items-center md:gap-2">
             <h2 className={smallTitle({ hiddenOnMobile: true })}>Rating</h2>
             <Rating value={rating} />
@@ -64,7 +64,7 @@ const BookDetailsPage: NextPage<
           <div className="col-start-1 col-end-3 row-start-2 row-end-2">
             <Link
               target="_blank"
-              className="inline-block rounded-lg bg-neutral-800 px-3 py-2 font-sans text-xs font-medium antialiased"
+              className="inline-block rounded-lg bg-neutral-800 px-3 py-2 font-sans text-xs font-medium antialiased md:text-sm"
               href={url}
             >
               Buy on Amazon
@@ -88,7 +88,7 @@ const BookDetailsPage: NextPage<
           {description && (
             <div className="col-start-1 col-end-3 row-start-3 row-end-3">
               <section className={readMore({ open })}>
-                <Paragraph>{description}</Paragraph>
+                <Paragraph className="mt-0">{description}</Paragraph>
               </section>
               {descriptionLength > MIN_DESCRIPTION_LENGTH && (
                 <button
