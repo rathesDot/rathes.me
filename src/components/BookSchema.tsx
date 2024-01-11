55d0821532fc78fb993014a8aa795e5d0ca155b0
Rathes Sachchithananthan <rathes@aheenam.com>
2024-01-11 12:03:55 +0000

    ✨ Introduce schema for book

---
+++ src/components/BookSchema.tsx
@@ -0,0 +1,39 @@
+import slugify from "slugify"
+
+import { Book } from "../utils/books"
+
+export const createSchemaForBook = (book: Book) => {
+  return {
+    "@context": "https://schema.org",
+    "@type": "Book",
+    name: book.title,
+    bookFormat: "Paperback",
+    inLanguage: book?.language || "English",
+    image: book.image,
+    description: book.description,
+    author: [{ "@type": "Person", name: book.author }],
+    review: {
+      reviewBody: book.description,
+      reviewRating: {
+        author: {
+          givenName: "Rathes",
+        },
+        bestRating: 5,
+        worstRating: 0,
+        ratingValue: book.rating,
+      },
+    },
+  }
+}
+
+export const BookSchema: React.FC<{ book: Book }> = ({ book }) => {
+  return (
+    <script
+      key={`book-${slugify(book.title)}`}
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(createSchemaForBook(book)),
+      }}
+    />
+  )
+}
