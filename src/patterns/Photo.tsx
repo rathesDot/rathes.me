8c12331cfc088d84a3486c1340aeb834193fec6e
Rathes Sachchithananthan <rathes@aheenam.com>
2024-02-15 08:09:37 +0000

    ✨ Make caption optional

--- src/patterns/Photo.tsx
+++ src/patterns/Photo.tsx
@@ -16,11 +16,15 @@ const container = cva("flex flex-col gap-1", {
 export const Photo: React.FC<{
   image: StaticImageData
   title: string
-  caption: string
+  caption?: string
   mode?: PhotoMode
 }> = ({ image, title, caption, mode = "portrait" }) => (
   <figure className={container({ mode })}>
     <Image src={image} alt={title} />
-    <figcaption className="px-4 text-xs text-neutral-500">{caption}</figcaption>
+    {caption && (
+      <figcaption className="px-4 text-xs text-neutral-500">
+        {caption}
+      </figcaption>
+    )}
   </figure>
 )
