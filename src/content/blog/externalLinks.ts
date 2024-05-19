4d1c47519f9cf14d564ebb5b7288b2f789865666
Rathes Sachchithananthan <rathes@aheenam.com>
2024-05-19 11:52:27 +0100

    ✨ Link to feature flag post

--- src/content/blog/externalLinks.ts
+++ src/content/blog/externalLinks.ts
@@ -1,5 +1,10 @@
 const links = [
   {
+    title: "A complete guide to feature flags",
+    date: "May 19, 2024",
+    link: "https://getkodi.com/blog/guide-to-feature-flags",
+  },
+  {
     title: "12 books in 12 months",
     date: "Dec 10, 2017",
     link: "https://medium.com/@rathes/12-books-in-12-months-54db49d8aaec",
