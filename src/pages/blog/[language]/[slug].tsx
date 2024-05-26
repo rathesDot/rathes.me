f641fe06cca692d8d648b050c4053bfc768aaaba
Rathes Sachchithananthan <rathes@aheenam.com>
2024-05-26 10:37:47 +0100

    💄 Update header

--- src/pages/blog/[language]/[slug].tsx
+++ src/pages/blog/[language]/[slug].tsx
@@ -6,6 +6,7 @@ import {
 } from "next"
 import Image from "next/image"
 import Link from "next/link"
+import { ComponentPropsWithRef } from "react"
 import { serialize } from "next-mdx-remote/serialize"
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
 import hightlight from "rehype-highlight"
@@ -13,6 +14,8 @@ import hightlight from "rehype-highlight"
 import path from "path"
 import fs from "fs"
 import matter from "gray-matter"
+
+import { format } from "date-fns"
 
 import books from "../../../../data/books.json"
 
@@ -38,9 +41,22 @@ import {
   Heading4,
 } from "../../../components"
 
+import { cva } from "class-variance-authority"
+
 const Pre = ({ children, className }) => {
   return <CodeBlock className={className}>{children}</CodeBlock>
 }
+
+const heading = cva("text-white antialiased mt-8", {
+  variants: {
+    level: {
+      h1: "text-2xl font-bold tracking-[-0.01em] leading-tight",
+      h2: "text-lg font-semibold",
+      h3: "text-base font-semibold",
+      h4: "text-base font-normal",
+    },
+  },
+})
 
 const Blogpost: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   source,
@@ -48,9 +64,12 @@ const Blogpost: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   title,
   excerpt,
   image,
+  date,
 }) => {
   const components: any = {
-    h1: Heading1,
+    h1: ({ className, ...props }: ComponentPropsWithRef<"h1">) => (
+      <h1 {...props} className={heading({ level: "h1", className })} />
+    ),
     h2: Heading2,
     h3: Heading3,
     h4: Heading4,
@@ -83,13 +102,27 @@ const Blogpost: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   return (
     <PageLayout>
       <Meta title={title} description={excerpt} meta={meta} />
-      <div className="max-w-[720px] p-8 md:px-14">
+      <article className="max-w-[54ch] p-8 md:px-14">
+        <header className="mb-8">
+          <h1 className={heading({ level: "h1" })}>{title}</h1>
+          <div className="flex items-center gap-2">
+            <time
+              className="text-sm text-neutral-400 antialiased"
+              dateTime={format(new Date(Date.parse(date)), "yyyy-MM-dd")}
+            >
+              {format(new Date(Date.parse(date)), "MMM dd, yyyy")}
+            </time>
+            <span className="text-sm text-neutral-600 antialiased">/</span>
+            <span className="text-sm text-neutral-400 antialiased">
+              Rathes Sachchithananthan
+            </span>
+          </div>
+        </header>
         {image && (
           <ImageWrapper aspect="16/9" className="mb-8">
             <Image src={image} fill sizes="704px" priority alt={title} />
           </ImageWrapper>
         )}
-        <Heading1 className="my-8">{title}</Heading1>
         <MDXRemote {...source} components={components} scope={{ books }} />
         <footer className="my-32">
           <Link
@@ -99,7 +132,7 @@ const Blogpost: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
             Back to all articles
           </Link>
         </footer>
-      </div>
+      </article>
     </PageLayout>
   )
 }
@@ -131,6 +164,7 @@ export const getStaticProps: GetStaticProps<{
   excerpt: string
   image?: string
   meta: { name: string; content: string }[]
+  date?: string
 }> = async ({ params }) => {
   const language = params.language
   const slug = params.slug
@@ -165,6 +199,7 @@ export const getStaticProps: GetStaticProps<{
         title: data.title,
         excerpt: excerpt || data.description,
         image: data.image || null,
+        date: data.date.toString() || null,
         meta: [...meta, ...imageMeta],
         url: `${SITE_URL}/blog/${language}/${slug}`,
         source: await serialize(content, {
