780437afd7fc905261e2538ee460c757e118dee2
Rathes Sachchithananthan <rathes@aheenam.com>
2024-05-28 12:29:07 +0100

    ✨ Introduce simpler navigation bar

---
+++ src/patterns/Navigation/Navbar.tsx
@@ -0,0 +1,34 @@
+import React, { ComponentPropsWithRef } from "react"
+import { cva } from "class-variance-authority"
+import Link from "next/link"
+
+const rootStyles = cva(
+  "bg-neutral-950/70 backdrop-blur-sm sticky top-0 border-b border-neutral-800"
+)
+
+export const Root: React.FC<ComponentPropsWithRef<"nav">> = ({
+  className,
+  children,
+  ...props
+}) => (
+  <nav className={rootStyles({ className })} {...props}>
+    <div className="container flex max-w-xl items-center justify-between gap-4 overflow-x-auto px-2 sm:px-6 md:px-12">
+      {children}
+    </div>
+  </nav>
+)
+
+const itemStyles = cva(
+  "text-neutral-400 text-sm flex-none font-defaultSans font-medium antialiased py-4 px-2 inline-flex transition-colors hover:text-white"
+)
+
+export const Item: React.FC<ComponentPropsWithRef<"a">> = ({
+  className,
+  children,
+  href,
+  ...props
+}) => (
+  <Link href={href} className={itemStyles({ className })} {...props}>
+    {children}
+  </Link>
+)
