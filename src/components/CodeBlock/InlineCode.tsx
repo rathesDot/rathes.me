05f5d866a2b672f180b497014cf18b1130881768
Rathes Sachchithananthan <rathes@aheenam.com>
2024-06-02 11:57:08 +0100

    🔥 Remove unused components --- These have all just been used in the blog post but now that they have been replaced, we don't need them anymore

--- src/components/CodeBlock/InlineCode.tsx
+++
@@ -1,8 +0,0 @@
-import { HTMLProps } from "react"
-
-export const InlineCode: React.FC<HTMLProps<HTMLElement>> = ({ ...props }) => (
-  <code
-    className="whitespace-normal break-all rounded-sm bg-neutral-800 px-1 py-[0.1em] font-mono text-sm text-neutral-300/80"
-    {...props}
-  />
-)
