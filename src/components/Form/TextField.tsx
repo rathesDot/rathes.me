b05de98af04290ff7b52a7446f4aa38b388a420a
Rathes Sachchithananthan <rathes@aheenam.com>
2024-06-02 11:58:25 +0100

    🏷️ Simplify type definitions

--- src/components/Form/TextField.tsx
+++ src/components/Form/TextField.tsx
@@ -1,8 +1,6 @@
-import { InputHTMLAttributes } from "react"
+import { ComponentPropsWithRef } from "react"
 
-export const TextField: React.FC<InputHTMLAttributes<HTMLInputElement>> = (
-  props
-) => (
+export const TextField: React.FC<ComponentPropsWithRef<"input">> = (props) => (
   <input
     {...props}
     className="box-border inline-flex h-9 w-full max-w-md items-center justify-center rounded-lg bg-neutral-800 px-2 text-base text-neutral-50/70 outline-none ring-1 ring-neutral-800 placeholder:text-neutral-500 focus:ring-neutral-700/50"
