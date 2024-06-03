b7afa3ee1fe8526ed651ba017bf6dff39ac4b527
Rathes Sachchithananthan <rathes@aheenam.com>
2024-06-03 06:47:04 +0100

    ✨ Set base font size

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithRef } from "react"
 import { cva } from "../../cva.config"
 
 const styles = cva({
-  base: "mb-4 text-neutral-400 antialiased last-of-type:mb-0",
+  base: "mb-4 text-neutral-400 antialiased text-base last-of-type:mb-0",
 })
 
 export const Paragraph: React.FC<ComponentPropsWithRef<"p">> = ({
