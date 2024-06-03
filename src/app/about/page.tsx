78309140497b40f0a662fe4b6467e7a5361c7d64
Rathes Sachchithananthan <rathes@aheenam.com>
2024-06-03 06:49:12 +0100

    🖊️ Add description for work experience --- Copying what is currently in my PDF resume

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -42,10 +42,16 @@ const AboutPage: NextPage = () => {
       <section className="mt-12 md:mt-16">
         <List.Root>
           <List.Title>Work Experience</List.Title>
-          <List.Container>
+          <List.Container className="space-y-6">
             <List.Item>
               Senior Engineer / Intercom
               <List.Subtitle>April 2024 — now</List.Subtitle>
+              <Paragraph className="mt-2 text-neutral-500">
+                Owning and leading bigger, more strategic projects at team level
+                and contributing to the roadmap and technical strategy. I'm also
+                coaching and mentoring other team mates to help the entire team
+                work as effective as possible
+              </Paragraph>
             </List.Item>
             <List.Item>
               Product Engineer 3 / Intercom
@@ -54,10 +60,21 @@ const AboutPage: NextPage = () => {
             <List.Item>
               Engineering Team Lead / Hussle
               <List.Subtitle>April 2021 — November 2021</List.Subtitle>
+              <Paragraph className="mt-2 text-neutral-500">
+                As the team could not grow due to the impacts of the pandemic, I
+                was guiding and leading the front-end and back-end engineers to
+                collaborate and work closely together to help out each other to
+                fill gaps
+              </Paragraph>
             </List.Item>
             <List.Item>
               Senior Frontend Engineer / Hussle
               <List.Subtitle>February 2020 — March 2021</List.Subtitle>
+              <Paragraph className="mt-2 text-neutral-500">
+                In 2020, I made the decision to make another big step and moved
+                to London to work in FitTech at Hussle. This was my chance to
+                combine my passion for fitness with my passion for tech.
+              </Paragraph>
             </List.Item>
             <List.Item>
               Software Engineer / Teamleader
@@ -66,6 +83,11 @@ const AboutPage: NextPage = () => {
             <List.Item>
               Founder / Aheenam
               <List.Subtitle>May 2016 — October 2021</List.Subtitle>
+              <Paragraph className="mt-2 text-neutral-500">
+                In 2016, I decided decrease client work and focus more on
+                creating software products. Therefore, I changed the type of my
+                business and created a custom brand for this company
+              </Paragraph>
             </List.Item>
             <List.Item>
               Web Developer / netinsiders
@@ -74,6 +96,12 @@ const AboutPage: NextPage = () => {
             <List.Item>
               Co-Founder / Kernwerk
               <List.Subtitle>October 2015 — January 2018</List.Subtitle>
+              <Paragraph className="mt-2 text-neutral-500">
+                Together with some friends, I created an awesome app for digital
+                personal training. I created the backend of first MVP using
+                Laravel as the framework of choice. Due to lack of time and
+                shift of interests, I left this project by the end of 2017.
+              </Paragraph>
             </List.Item>
             <List.Item>
               Product Assistant / Kopierland GmbH
@@ -82,6 +110,11 @@ const AboutPage: NextPage = () => {
             <List.Item>
               Web Designer / rathesDot (CreativeCreation)
               <List.Subtitle>June 2006 — March 2016</List.Subtitle>
+              <Paragraph className="mt-2 text-neutral-500">
+                Earning my first money creating custom MySpace layouts, I went
+                on to create custom websites for various customer of different
+                sizes as a freelancer
+              </Paragraph>
             </List.Item>
           </List.Container>
         </List.Root>
