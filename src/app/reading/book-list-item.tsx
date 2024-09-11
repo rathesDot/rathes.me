431359bd0eff811ff795f079be80aab50342ff18
Rathes Sachchithananthan <rathes@aheenam.com>
2024-09-11 08:19:57 +0100

    ♻️ Separate both cases

--- src/app/reading/book-list-item.tsx
+++ src/app/reading/book-list-item.tsx
@@ -5,13 +5,24 @@ import React from "react"
 import * as List from "../../components/List"
 import { Book, createSlug } from "../../services/books"
 
-export const BookListItem: React.FC<{ book: Book }> = ({ book }) => (
-  <List.Item>
-    <List.Link
-      href={!!book?.rating ? `/reading/${createSlug(book)}` : book.url}
-    >
-      {book.title}
-      <List.Subtitle>by {book.author}</List.Subtitle>
-    </List.Link>
-  </List.Item>
-)
+export const BookListItem: React.FC<{ book: Book }> = ({ book }) => {
+  if (!!book?.rating) {
+    return (
+      <List.Item>
+        <List.Link href={`/reading/${createSlug(book)}`}>
+          {book.title}
+          <List.Subtitle>by {book.author}</List.Subtitle>
+        </List.Link>
+      </List.Item>
+    )
+  }
+
+  return (
+    <List.Item>
+      <List.Link href={book.url}>
+        {book.title}
+        <List.Subtitle>by {book.author}</List.Subtitle>
+      </List.Link>
+    </List.Item>
+  )
+}
