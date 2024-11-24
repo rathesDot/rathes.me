2fcd8a26fc563b9e1e28bcba2bd87852a537017d
Rathes Sachchithananthan <rathes@aheenam.com>
2024-11-24 19:50:52 +0000

    🔥 Remove dark mode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const plugin = require("tailwindcss/plugin")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   safelist: [
     {
