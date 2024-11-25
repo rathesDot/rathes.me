9cc699d22f6f86353735f601ea73a63789a29e10
Rathes Sachchithananthan <rathes@aheenam.com>
2024-11-25 12:39:43 +0000

    🔥 Remove rounded corners for photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,28 +161,13 @@ const HomePage: NextPage = () => {
         <div className="-mr-4">
           <div className="flex flex-nowrap items-start gap-4 overflow-x-auto pr-4 pb-2">
             <div className="flex-none">
-              <Image
-                src={Iceland1}
-                width={210}
-                alt="work 1"
-                className="rounded-lg object-cover"
-              />
+              <Image src={Iceland1} width={210} alt="work 1" />
             </div>
             <div className="flex-none">
-              <Image
-                src={Iceland2}
-                width={210}
-                className="rounded-lg"
-                alt="work 2"
-              />
+              <Image src={Iceland2} width={210} alt="work 2" />
             </div>
             <div className="flex-none">
-              <Image
-                src={Iceland3}
-                width={210}
-                className="rounded-lg"
-                alt="work 3"
-              />
+              <Image src={Iceland3} width={210} alt="work 3" />
             </div>
           </div>
         </div>
