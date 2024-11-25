e69bac3a2485242d521833b8b1c96c482b23662c
Rathes Sachchithananthan <rathes@aheenam.com>
2024-11-25 17:26:06 +0000

    💄 Basic blog styles

--- src/app/blog/[lang]/[slug]/components.tsx
+++ src/app/blog/[lang]/[slug]/components.tsx
@@ -19,24 +19,26 @@ const image = cva({
   base: "relative block my-4 [&_img]:object-cover [&_img]:object-center",
 })
 
-export const caption = cva({ base: "text-neutral-400 block my-1 text-xs" })
+export const caption = cva({
+  base: "text-neutral-500 dark:text-neutral-400 block my-1 text-xs",
+})
 
 const orderedList = cva({
-  base: "list-decimal pl-6 my-4 space-y-4 marker:text-neutral-400/70",
+  base: "list-decimal pl-6 my-4 space-y-4 marker:text-neutral-400",
 })
 const unorderedList = cva({
-  base: "list-disc pl-6 my-4 space-y-4 marker:text-neutral-400/70",
+  base: "list-disc pl-6 my-4 space-y-4 marker:text-neutral-400",
 })
-const listItem = cva({ base: "text-neutral-400 antialiased [&_p]:mb-0" })
+const listItem = cva({ base: "antialiased [&_p]:mb-0" })
 
-const bold = cva({ base: "text-neutral-300 font-semibold antialiased" })
+const bold = cva({ base: "font-semibold antialiased" })
 
 const strikethrough = cva({
-  base: "line-through decoration-neutral-400 hover:decoration-neutral-400/50 transition-colors font-medium antialiased mb-4",
+  base: "line-through decoration-neutral-400 transition-colors font-medium antialiased mb-4",
 })
 
 const quote = cva({
-  base: "relative my-8 py-8 pl-12 pr-8 bg-neutral-800/30 text-neutral-400",
+  base: "relative my-8 py-8 pl-12 pr-8 bg-neutral-50 text-neutral-500 outline outline-neutral-200 dark:bg-neutral-800 dark:text-neutral-400 dark:outline-neutral-800 rounded-sm",
 })
 
 const inlineCode = cva({
@@ -50,11 +52,11 @@ const inlineCode = cva({
 })
 
 const codeBlock = cva({
-  base: "my-8 overflow-x-auto rounded-sm bg-neutral-800/30 p-8 text-neutral-400 outline outline-neutral-800",
+  base: "my-8 overflow-x-auto rounded-sm bg-neutral-50 p-8 text-neutral-500 outline outline-neutral-200 dark:bg-neutral-800 dark:text-neutral-400 dark:outline-neutral-800",
 })
 
 const note = cva({
-  base: "my-8 p-8 bg-neutral-800/30 outline outline-neutral-800 text-neutral-400 rounded-sm",
+  base: "my-8 p-8 bg-neutral-50 text-neutral-500 outline outline-neutral-200 dark:bg-neutral-800 dark:text-neutral-400 dark:outline-neutral-800 rounded-sm",
   variants: {
     fontFamily: {
       mono: "font-mono",
@@ -90,7 +92,7 @@ export const components: any = {
         viewBox="0 0 41 33"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="absolute left-6 top-6"
+        className="absolute top-6 left-6"
       >
         <path
           d="M40.232 17.984V32.832H24.36V23.104C24.36 18.4106 24.872 14.4426 25.896 11.2C26.92 7.87196 28.456 4.45862 30.504 0.959961H38.056C37.288 3.00796 36.5626 4.92796 35.88 6.71996C35.2826 8.51196 34.7706 10.3466 34.344 12.224C34.0026 14.016 33.832 15.936 33.832 17.984H40.232ZM16.04 17.984V32.832H0.167969V23.104C0.167969 18.4106 0.679969 14.4426 1.70397 11.2C2.8133 7.87196 4.3493 4.45862 6.31197 0.959961H13.864C13.0106 3.00796 12.2853 4.92796 11.688 6.71996C11.0906 8.51196 10.6213 10.3466 10.28 12.224C9.93864 14.016 9.76797 15.936 9.76797 17.984H16.04Z"
