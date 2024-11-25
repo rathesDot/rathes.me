3dde051c7e1111c5c5acdd466192524f196bfa7a
Rathes Sachchithananthan <rathes@aheenam.com>
2024-11-25 10:21:23 +0000

    💄 Add base styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable}`}>
-      <body>
+      <body className="bg-white font-sans text-neutral-950 dark:bg-neutral-950 dark:text-neutral-50">
         <Fathom />
         {children}
       </body>
