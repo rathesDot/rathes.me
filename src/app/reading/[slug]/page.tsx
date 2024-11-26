43a107773f575175aedb0e3c49eb0039eead5017
Rathes Sachchithananthan <rathes@aheenam.com>
2024-11-26 10:05:48 +0000

    💄 Set base styles

--- src/app/reading/[slug]/page.tsx
+++ src/app/reading/[slug]/page.tsx
@@ -53,50 +53,48 @@ const BookDetailsPage: NextPage<{ params: Params }> = async (props) => {
   const { title, author, url, description, genres, rating } = book
 
   return (
-    <div>
+    <main className="space-y-4 py-4">
       <BookSchema book={book} />
-      <div className="flex max-w-xl flex-col gap-4 md:gap-6">
-        <header>
-          <Heading1>{title}</Heading1>
-          <p className="text-base text-neutral-300/80">by {author}</p>
-        </header>
-        <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-x-2 md:gap-y-6">
-          <div className="col-start-1 col-end-1 row-start-1 row-end-1 md:flex md:items-center md:gap-2">
-            <h2 className={smallTitle({ hiddenOnMobile: true })}>Rating</h2>
-            <Rating value={rating || 0} />
-          </div>
-          <div className="col-start-1 col-end-3 row-start-2 row-end-2">
-            <Link
-              target="_blank"
-              className="inline-block rounded-lg bg-neutral-800 px-3 py-2 font-sans text-xs font-medium antialiased md:text-sm"
-              href={url}
-            >
-              Buy on Amazon
-            </Link>
-          </div>
-          <div className="col-start-2 col-end-2 row-start-1 row-end-1 flex items-center gap-2">
-            <h2 className={smallTitle({ hiddenOnMobile: true })}>Genres</h2>
-            <div className="flex gap-2">
-              {(genres || []).map((genre) => (
-                <Link
-                  key={genre.toLocaleLowerCase()}
-                  underlined
-                  className="text-xs underline-offset-2 antialiased md:text-sm"
-                  href={`/reading?genre=${genre.toLowerCase()}`}
-                >
-                  {genre}
-                </Link>
-              ))}
-            </div>
-          </div>
-          {description && (
-            <div className="col-start-1 col-end-3 row-start-3 row-end-3">
-              <Paragraph className="mt-0">{description}</Paragraph>
-            </div>
-          )}
+      <header>
+        <Heading1>{title}</Heading1>
+        <p className="text-base text-neutral-300/80">by {author}</p>
+      </header>
+      <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-x-2 md:gap-y-6">
+        <div className="col-start-1 col-end-1 row-start-1 row-end-1 md:flex md:items-center md:gap-2">
+          <h2 className={smallTitle({ hiddenOnMobile: true })}>Rating</h2>
+          <Rating value={rating || 0} />
         </div>
+        <div className="col-start-1 col-end-3 row-start-2 row-end-2">
+          <Link
+            target="_blank"
+            className="inline-block rounded-lg bg-neutral-800 px-3 py-2 font-sans text-xs font-medium antialiased md:text-sm"
+            href={url}
+          >
+            Buy on Amazon
+          </Link>
+        </div>
+        <div className="col-start-2 col-end-2 row-start-1 row-end-1 flex items-center gap-2">
+          <h2 className={smallTitle({ hiddenOnMobile: true })}>Genres</h2>
+          <div className="flex gap-2">
+            {(genres || []).map((genre) => (
+              <Link
+                key={genre.toLocaleLowerCase()}
+                underlined
+                className="text-xs underline-offset-2 antialiased md:text-sm"
+                href={`/reading?genre=${genre.toLowerCase()}`}
+              >
+                {genre}
+              </Link>
+            ))}
+          </div>
+        </div>
+        {description && (
+          <div className="col-start-1 col-end-3 row-start-3 row-end-3">
+            <Paragraph className="mt-0">{description}</Paragraph>
+          </div>
+        )}
       </div>
-    </div>
+    </main>
   )
 }
 
