5f021aaad2826bdff81d97baf6188346386e00f4
Rathes Sachchithananthan <rathes@aheenam.com>
2024-11-27 12:55:49 +0000

    ✨ Add header

--- src/app/links/layout.tsx
+++ src/app/links/layout.tsx
@@ -1,5 +1,10 @@
 import { Link } from "@/components/Link"
 
+import Image from "next/image"
+import Profile from "@/assets/images/profile-picture.png"
+import { Heading1 } from "@/src/components/Heading"
+import { Paragraph } from "@/src/components/Paragraph"
+
 export default function RootLayout({
   children,
 }: {
@@ -7,6 +12,19 @@ export default function RootLayout({
 }) {
   return (
     <>
+      <header className="mx-auto max-w-lg space-y-8 py-12 text-center">
+        <Image
+          src={Profile}
+          alt="Profile picture of Rathes Sachchithananthan"
+          className="mx-auto size-24 rounded-full"
+        />
+        <div>
+          <Heading1 level="h2">Rathes Sachchithananthan</Heading1>
+          <Paragraph className="mx-auto my-1 max-w-sm text-balance">
+            Web engineer interested in languages, fitness, reading and cooking.
+          </Paragraph>
+        </div>
+      </header>
       {children}
       <footer className="mx-auto max-w-lg space-y-12 px-4 py-8 text-center sm:px-0">
         <div className="text-xs text-neutral-600 dark:text-neutral-400">
